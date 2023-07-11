dbc5e3fb0ad9dbae6fec83676b926d8e2ddbbc90
Go-Jaecheol <gojaech@naver.com>
2023-07-11 16:53:49 +0900

    chore: 백엔드 프로젝트 초기 세팅

---
+++ backend/src/main/java/com/funeat/FuneatApplication.java
@@ -0,0 +1,13 @@
+package com.funeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuneatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuneatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/funeat/FuneatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuneatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
