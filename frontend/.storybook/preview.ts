f8057b6bc96ab4b814e71f045f45531f709c62ac
TaeeunKim <xodms0309@naver.com>
2023-07-13 13:31:05 +0900

    feat: storybook에 msw 적용
    Co-authored-by: Leejin Yang <ejin1996@gmail.com>
    Co-authored-by: sᴏʟʙɪ ☔️ <solbi2004@naver.com>

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from '@storybook/react';
+import mswDecorator from 'msw-storybook-addon';
+import { handlers } from '../src/mocks/handlers';
+
+export const decorators = [mswDecorator];
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +13,7 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    msw: handlers,
   },
 };
 
