f2f7098b03d368cb9c5ecfed22bf6b2d86dd628e
Taeeun Kim <xodms0309@naver.com>
2023-07-13 13:32:21 +0900

    [FE] chore: msw 설정 (#19)
    * chore: msw 설치
    
    Co-authored-by: Leejin Yang <ejin1996@gmail.com>
    Co-authored-by: sᴏʟʙɪ ☔️ <solbi2004@naver.com>
    
    * feat: msw 적용
    
    Co-authored-by: Leejin Yang <ejin1996@gmail.com>
    Co-authored-by: sᴏʟʙɪ ☔️ <solbi2004@naver.com>
    
    * feat: storybook에 msw 적용
    
    Co-authored-by: Leejin Yang <ejin1996@gmail.com>
    Co-authored-by: sᴏʟʙɪ ☔️ <solbi2004@naver.com>
    
    ---------
    
    Co-authored-by: Leejin Yang <ejin1996@gmail.com>
    Co-authored-by: sᴏʟʙɪ ☔️ <solbi2004@naver.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const main = async () => {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./mocks/browser');
+    await worker.start();
+  }
+};
+await main();
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
