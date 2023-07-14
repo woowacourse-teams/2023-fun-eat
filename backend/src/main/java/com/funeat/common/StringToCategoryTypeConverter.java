0199b12986425d4d2675310beaa0981e5e0b04fd
Go-Jaecheol <gojaech@naver.com>
2023-07-14 15:16:50 +0900

    feat: string으로 들어온 CategoryType을 enum으로 변환해주는 Converter 추가

---
+++ backend/src/main/java/com/funeat/common/StringToCategoryTypeConverter.java
@@ -0,0 +1,11 @@
+package com.funeat.common;
+
+import com.funeat.product.domain.CategoryType;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToCategoryTypeConverter implements Converter<String, CategoryType> {
+    @Override
+    public CategoryType convert(final String source) {
+        return CategoryType.valueOf(source.toUpperCase());
+    }
+}
