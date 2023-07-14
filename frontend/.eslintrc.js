a8f85faf42100af4e6439e8ad2571707436841dc
TaeeunKim <xodms0309@naver.com>
2023-07-14 21:31:42 +0900

    chore: import 순서 lint 적용

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,40 +1,85 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+    'plugin:import/recommended',
+  ],
   ignorePatterns: ['*.js'],
-  overrides: [{
-    env: {
-      node: true
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    files: ['.eslintrc.{js,cjs}'],
-    parserOptions: {
-      sourceType: 'script'
-    }
-  }],
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
-    tsconfigRootDir: __dirname
+    tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'import'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/consistent-type-imports': ['error', {
-      prefer: 'type-imports',
-      disallowTypeAnnotations: false
-    }],
-    'react/jsx-key': ['error', {
-      warnOnDuplicates: true
-    }],
-    'react/self-closing-comp': ['error', {
-      component: true,
-      html: true
-    }]
-  }
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: false,
+      },
+    ],
+    'react/jsx-key': [
+      'error',
+      {
+        warnOnDuplicates: true,
+      },
+    ],
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'object', 'unknown'],
+        pathGroups: [
+          {
+            pattern: '@storybook/**',
+            group: 'external',
+          },
+          {
+            pattern: '@*',
+            group: 'unknown',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['unknown'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      webpack: {},
+    },
+  },
 };
