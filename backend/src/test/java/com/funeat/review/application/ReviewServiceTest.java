7b959f34196c5a690e465e26343312b02bcb3c79
wugawuga <dnr3655@naver.com>
2023-07-15 21:51:10 +0900

    refactor: 리뷰서비스 테스트 조건 수정

--- backend/src/test/java/com/funeat/review/application/ReviewServiceTest.java
+++ backend/src/test/java/com/funeat/review/application/ReviewServiceTest.java
@@ -12,30 +12,21 @@ import com.funeat.review.persistence.ReviewRepository;
 import com.funeat.review.presentation.dto.ReviewCreateRequest;
 import com.funeat.tag.domain.Tag;
 import com.funeat.tag.persistence.TagRepository;
-import io.restassured.RestAssured;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+@SpringBootTest
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(ReplaceUnderscores.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ReviewServiceTest {
-
-    @LocalServerPort
-    private int port;
-
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
 
     @Autowired
     private ReviewService reviewService;
@@ -58,8 +49,11 @@ class ReviewServiceTest {
         var member = 멤버_추가_요청();
         var product = 상품_추가_요청();
         var tags = 태그_추가_요청();
+        var tagIds = tags.stream()
+                .map(Tag::getId)
+                .collect(Collectors.toList());
         var image = 리뷰_페이크_사진_요청();
-        var request = new ReviewCreateRequest(4.5, List.of(tags.get(0).getId(), tags.get(1).getId()), "review", true,
+        var request = new ReviewCreateRequest(4.5, tagIds, "review", true,
                 member.getId());
 
         // when
@@ -69,6 +63,7 @@ class ReviewServiceTest {
         // then
         assertThat(result.get(0)).usingRecursiveComparison()
                 .ignoringExpectedNullFields()
+                .comparingOnlyFields("member", "product", "image", "rating", "content", "reBuy")
                 .isEqualTo(
                         new Review(member, product, image.getOriginalFilename(), 4.5, "review", true)
                 );
