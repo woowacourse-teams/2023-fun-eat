a22886567aecdd7d076cc405f9b9aa28075c550e
TaeeunKim <xodms0309@naver.com>
2023-07-15 16:19:36 +0900

    feat: SvgIcon 컴포넌트 추가

---
+++ frontend/src/components/Common/Svg/SvgIcon.stories.tsx
@@ -0,0 +1,37 @@
+import { theme } from '@fun-eat/design-system';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import SvgIcon, { SvgIconVariant } from './SvgIcon';
+
+const meta: Meta<typeof SvgIcon> = {
+  title: 'common/SvgIcon',
+  component: SvgIcon,
+  argTypes: {
+    color: {
+      control: {
+        type: 'color',
+      },
+    },
+  },
+  args: {
+    variant: 'recipe',
+    color: theme.colors.gray4,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {};
+
+export const SvgIcons: Story = {
+  render: () => {
+    return (
+      <>
+        {Object.values(SvgIconVariant).map((variant) => (
+          <SvgIcon key={variant} variant={variant} />
+        ))}
+      </>
+    );
+  },
+};
