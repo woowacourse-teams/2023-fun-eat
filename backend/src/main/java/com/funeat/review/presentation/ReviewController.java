d5fc1eebc00438f84021634663aaeee16b3d30e4
70825 <hello70825@gmail.com>
2023-07-16 00:07:44 +0900

    feat: 리뷰 목록 조회 컨트롤러 추가

--- backend/src/main/java/com/funeat/review/presentation/ReviewController.java
+++ backend/src/main/java/com/funeat/review/presentation/ReviewController.java
@@ -2,7 +2,9 @@ package com.funeat.review.presentation;
 
 import com.funeat.review.application.ReviewService;
 import com.funeat.review.presentation.dto.ReviewCreateRequest;
+
 import java.net.URI;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.funeat.review.application.ReviewService;
+import com.funeat.review.presentation.dto.SortingReviewDto;
+import com.funeat.review.presentation.dto.SortingReviewsPageDto;
+import com.funeat.review.presentation.dto.SortingReviewsResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ReviewController {
@@ -22,10 +38,22 @@ public class ReviewController {
 
     @PostMapping(value = "/api/products/{productId}/reviews", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE,
             MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Void> writeReview(@PathVariable Long productId, @RequestPart MultipartFile image,
+    public ResponseEntity<Void> writeReview(@PathVariable Long productId,
+                                            @RequestPart MultipartFile image,
                                             @RequestPart ReviewCreateRequest reviewRequest) {
         reviewService.create(productId, image, reviewRequest);
 
         return ResponseEntity.created(URI.create("/api/products/" + productId)).build();
+    }
+
+    @GetMapping(value = "/api/products/{productId}/reviews")
+    public ResponseEntity<SortingReviewsResponse> getSortingReviews(@PathVariable Long productId,
+                                                                    @RequestParam String option,
+                                                                    @PageableDefault Pageable pageable) {
+        final SortingReviewsPageDto sortingReviewsPageDto = reviewService.computePageInfo(pageable, productId);
+        final List<SortingReviewDto> reviews = reviewService.sortingReviews(productId, pageable, option);
+        final SortingReviewsResponse response = new SortingReviewsResponse(sortingReviewsPageDto, reviews);
+
+        return ResponseEntity.ok(response);
     }
 }
