1e1642fa3bd3471ef414d9be6f7ef027fcc5d52a
wugawuga <dnr3655@naver.com>
2023-07-16 14:16:17 +0900

    feat: 이미지 업로드 시 빈파일 생성 이슈 해결
    - dev, test 분리하여 test 시에는 임시 폴더, 파일생성 후 삭제로직 구현

--- backend/src/main/java/com/funeat/review/application/ReviewService.java
+++ backend/src/main/java/com/funeat/review/application/ReviewService.java
@@ -11,13 +11,8 @@ import com.funeat.review.persistence.ReviewTagRepository;
 import com.funeat.review.presentation.dto.ReviewCreateRequest;
 import com.funeat.tag.domain.Tag;
 import com.funeat.tag.persistence.TagRepository;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,23 +21,22 @@ import org.springframework.web.multipart.MultipartFile;
 @Transactional(readOnly = true)
 public class ReviewService {
 
-    @Value("${review.image.path}")
-    private String imagePath;
-
     private final ReviewRepository reviewRepository;
     private final TagRepository tagRepository;
     private final ReviewTagRepository reviewTagRepository;
     private final MemberRepository memberRepository;
     private final ProductRepository productRepository;
+    private final ImageService imageService;
 
     public ReviewService(final ReviewRepository reviewRepository, final TagRepository tagRepository,
                          final ReviewTagRepository reviewTagRepository, final MemberRepository memberRepository,
-                         final ProductRepository productRepository) {
+                         final ProductRepository productRepository, final ImageService imageService) {
         this.reviewRepository = reviewRepository;
         this.tagRepository = tagRepository;
         this.reviewTagRepository = reviewTagRepository;
         this.memberRepository = memberRepository;
         this.productRepository = productRepository;
+        this.imageService = imageService;
     }
 
     @Transactional
@@ -52,7 +46,6 @@ public class ReviewService {
                 .orElseThrow(IllegalArgumentException::new);
         final Product findProduct = productRepository.findById(productId)
                 .orElseThrow(IllegalArgumentException::new);
-        writeImage(image);
 
         final Review savedReview = reviewRepository.save(
                 new Review(findMember, findProduct, image.getOriginalFilename(), reviewRequest.getRating(),
@@ -64,16 +57,7 @@ public class ReviewService {
                 .map(findTag -> new ReviewTag(savedReview, findTag))
                 .collect(Collectors.toList());
 
+        imageService.upload(image);
         reviewTagRepository.saveAll(reviewTags);
-    }
-
-    private void writeImage(final MultipartFile image) {
-        final String originalImageName = image.getOriginalFilename();
-        final Path path = Paths.get(imagePath + originalImageName);
-        try {
-            Files.write(path, image.getBytes());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
