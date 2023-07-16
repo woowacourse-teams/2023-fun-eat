324647f2ed09d7861beea82aa2c96d5b11582505
Leejin-Yang <ejin1996@gmail.com>
2023-07-16 18:40:42 +0900

    feat: ProductItem 스토리북 args 추가

--- frontend/src/components/Product/ProductItem/ProductItem.stories.tsx
+++ frontend/src/components/Product/ProductItem/ProductItem.stories.tsx
@@ -1,10 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import ProductItem from './ProductItem';
+import type { Product } from '../mock';
+import productImage from '../mock_img.jpg';
+
+const MOCK_PRODUCT: Product = {
+  id: 1,
+  name: '꼬북칩',
+  price: 1500,
+  image: productImage,
+  averageRating: 4.5,
+  reviewCount: 100,
+};
 
 const meta: Meta<typeof ProductItem> = {
   title: 'product/ProductItem',
   component: ProductItem,
+  args: {
+    product: MOCK_PRODUCT,
+  },
 };
 
 export default meta;
