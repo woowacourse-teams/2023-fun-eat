11d0ef89b67737bdced8db759a5ef406c18aa206
wugawuga <dnr3655@naver.com>
2023-07-17 01:22:08 +0900

    refactor: ReviewTag 조회 시, 매핑된 엔티티 fetch 전략 수정

--- backend/src/main/java/com/funeat/review/domain/ReviewTag.java
+++ backend/src/main/java/com/funeat/review/domain/ReviewTag.java
@@ -2,6 +2,7 @@ package com.funeat.review.domain;
 
 import com.funeat.tag.domain.Tag;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,11 +16,11 @@ public class ReviewTag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id")
     private Review review;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tag_id")
     private Tag tag;
 
