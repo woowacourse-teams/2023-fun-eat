359d0bf7244c4a68d010f51885ab97f0129b45b2
70825 <hello70825@gmail.com>
2023-07-17 08:42:31 +0900

    refactor: 주석 삭제

--- backend/src/main/java/com/funeat/review/presentation/dto/RankingReviewsResponse.java
+++ backend/src/main/java/com/funeat/review/presentation/dto/RankingReviewsResponse.java
@@ -1,19 +1,7 @@
 package com.funeat.review.presentation.dto;
 
-import com.funeat.review.domain.Review;
-
 import java.util.List;
 
-/**
- * {
- * 	    "reviewId": 1,
- * 			"productId": 5,
- * 	    "productName": "구운감자슬림명란마요",
- * 			"content": "할머니가 먹을 거 같은 맛입니다.",
- * 	    "rating": 4.0,
- * 			"favoriteCount": 1256
- *            }
- */
 public class RankingReviewsResponse {
 
     private List<RankingReviewDto> reviews;
