8434ab340e59f924edafb8c676f0fddba0d95fc4
70825 <hello70825@gmail.com>
2023-07-17 08:34:27 +0900

    feat: 리뷰 랭킹 조회 컨트롤러 구현

--- backend/src/main/java/com/funeat/review/presentation/ReviewController.java
+++ backend/src/main/java/com/funeat/review/presentation/ReviewController.java
@@ -1,12 +1,17 @@
 package com.funeat.review.presentation;
 
 import com.funeat.review.application.ReviewService;
+import com.funeat.review.domain.Review;
+import com.funeat.review.presentation.dto.RankingReviewDto;
+import com.funeat.review.presentation.dto.RankingReviewsResponse;
 import com.funeat.review.presentation.dto.ReviewCreateRequest;
 import com.funeat.review.presentation.dto.ReviewFavoriteRequest;
-import java.net.URI;
-
+import com.funeat.review.presentation.dto.SortingReviewsResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,12 +20,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.funeat.review.presentation.dto.SortingReviewsResponse;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ReviewController {
@@ -47,10 +49,23 @@ public class ReviewController {
 
         return ResponseEntity.noContent().build();
 
+    }
+
     @GetMapping(value = "/api/products/{productId}/reviews")
     public ResponseEntity<SortingReviewsResponse> getSortingReviews(@PathVariable Long productId,
                                                                     @PageableDefault Pageable pageable) {
         final SortingReviewsResponse response = reviewService.sortingReviews(productId, pageable);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/api/ranks/reviews")
+    public ResponseEntity<RankingReviewsResponse> getRankingReviews() {
+        final List<Review> reviews = reviewService.computeRankingReviews();
+        final List<RankingReviewDto> dtos = reviews.stream()
+                .map(RankingReviewDto::toDto)
+                .collect(Collectors.toList());
+        final RankingReviewsResponse response = RankingReviewsResponse.toResponse(dtos);
 
         return ResponseEntity.ok(response);
     }
