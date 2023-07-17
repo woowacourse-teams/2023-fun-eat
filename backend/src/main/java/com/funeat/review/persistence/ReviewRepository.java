679cf66e8ff44bc8a04626b396847e464103623c
hanueleee <ezzeezz@naver.com>
2023-07-17 19:39:38 +0900

    refactor: ReviewRepository의 countByProduct 수정

--- backend/src/main/java/com/funeat/review/persistence/ReviewRepository.java
+++ backend/src/main/java/com/funeat/review/persistence/ReviewRepository.java
@@ -1,15 +1,10 @@
 package com.funeat.review.persistence;
 
-import com.funeat.product.domain.Category;
 import com.funeat.product.domain.Product;
+import com.funeat.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ReviewRepository extends JpaRepository<Category, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("SELECT COUNT(r) FROM Review r WHERE r.product = :product")
-    Long countByProduct(@Param("product") Product product);
+    Long countByProduct(final Product product);
 }
