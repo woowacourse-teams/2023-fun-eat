17c2706f3cc29652de2dbacb3c6c9ef37b2551e3
hae-on <solbi2004@naver.com>
2023-07-17 14:18:52 +0900

    feat: BottomSheetContent 구현

---
+++ frontend/src/components/Common/BottomSheetContent/BottomSheetContent.stories.tsx
@@ -0,0 +1,33 @@
+import { BottomSheet } from '@fun-eat/design-system';
+import type { Meta, StoryObj } from '@storybook/react';
+import { useEffect, useRef } from 'react';
+
+import BottomSheetContent from './BottomSheetContent';
+
+const meta: Meta<typeof BottomSheetContent> = {
+  title: 'BottomSheetContent',
+  component: BottomSheetContent,
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    const ref = useRef<HTMLDialogElement>(null);
+
+    useEffect(() => {
+      ref.current?.showModal();
+    }, []);
+
+    const closeBottomSheet = () => {
+      ref.current?.close();
+    };
+
+    return (
+      <BottomSheet ref={ref} close={closeBottomSheet}>
+        <BottomSheetContent close={closeBottomSheet} />
+      </BottomSheet>
+    );
+  },
+};
