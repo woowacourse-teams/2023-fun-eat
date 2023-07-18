b00460d886cb78ba7e84eba02b78f46052c416bf
Leejin-Yang <ejin1996@gmail.com>
2023-07-18 13:07:07 +0900

    feat: ProductDetail 컴포넌트 추가

---
+++ frontend/src/components/Product/ProductDetail/ProductDetail.tsx
@@ -0,0 +1,86 @@
+import { Badge, Text } from '@fun-eat/design-system';
+import styled, { useTheme } from 'styled-components';
+
+import { SvgIcon } from '@/components/Common';
+import productDetail from '@/mocks/data/productDetail.json';
+
+const ProductDetail = () => {
+  const { price, image, content, averageRating, tags } = productDetail;
+  const theme = useTheme();
+
+  return (
+    <ProductDetailContainer>
+      <img src={image} width={300} />
+      <DetailInfoWrapper>
+        <DescriptionWrapper>
+          <Text weight="bold">가격</Text>
+          <Text>{price.toLocaleString('ko-KR')}원</Text>
+        </DescriptionWrapper>
+        <DescriptionWrapper>
+          <Text weight="bold">상품 설명</Text>
+          <Text>{content}</Text>
+        </DescriptionWrapper>
+        <DescriptionWrapper>
+          <Text weight="bold">평균 평점</Text>
+          <RatingIconWrapper>
+            <SvgIcon variant="star" width={20} height={20} color={theme.colors.secondary} />
+            <Text as="span">{averageRating}</Text>
+          </RatingIconWrapper>
+        </DescriptionWrapper>
+      </DetailInfoWrapper>
+      <TagList>
+        {tags.map((tag) => (
+          <li key={tag.id}>
+            <Badge color="black" textColor="white">
+              {tag.name}
+            </Badge>
+          </li>
+        ))}
+      </TagList>
+    </ProductDetailContainer>
+  );
+};
+
+export default ProductDetail;
+
+const ProductDetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 30px;
+
+  & > img {
+    align-self: center;
+  }
+`;
+
+const DetailInfoWrapper = styled.div`
+  & > div + div {
+    margin-top: 10px;
+  }
+`;
+
+const DescriptionWrapper = styled.div`
+  display: flex;
+  column-gap: 20px;
+
+  & > p:first-of-type {
+    flex-shrink: 0;
+    width: 60px;
+  }
+`;
+
+const RatingIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 4px;
+  margin-left: -4px;
+
+  & > svg {
+    padding-bottom: 2px;
+  }
+`;
+
+const TagList = styled.ul`
+  display: flex;
+  column-gap: 8px;
+`;
