3aefcb7206f4fce3e48eb10078e2a195f9c517d2
Leejin-Yang <ejin1996@gmail.com>
2023-07-19 02:04:49 +0900

    feat: ReviewItem 컴포넌트 추가

---
+++ frontend/src/components/Review/ReviewItem/ReviewItem.tsx
@@ -0,0 +1,101 @@
+import { Badge, Text, useTheme } from '@fun-eat/design-system';
+import styled from 'styled-components';
+
+import { SvgIcon } from '@/components/Common';
+import TagList from '@/components/Common/TagList/TagList';
+import type { Review } from '@/types/review';
+
+interface ReviewItemProps {
+  product: string;
+  review: Review;
+}
+
+const ReviewItem = ({ product, review }: ReviewItemProps) => {
+  const { userName, profileImage, image, rating, tags, content, rebuy, favoriteCount, favorite } = review;
+  const theme = useTheme();
+
+  return (
+    <ReviewItemContainer>
+      <ReviewerWrapper>
+        <ReviewerInfoWrapper>
+          <img src={profileImage} width={40} height={40} alt={`${userName}의 프로필`} />
+          <div>
+            <Text weight="bold">{userName}</Text>
+            <RatingIconWrapper>
+              <SvgIcon variant="star" color={theme.colors.secondary} width={16} height={16} />
+              <Text size="xs">{rating.toFixed(1)}</Text>
+            </RatingIconWrapper>
+          </div>
+        </ReviewerInfoWrapper>
+        {rebuy && (
+          <Badge
+            color={theme.colors.primary}
+            textColor={theme.textColors.default}
+            css={`
+              font-weight: ${theme.fontWeights.bold};
+            `}
+          >
+            😝 또 살래요
+          </Badge>
+        )}
+      </ReviewerWrapper>
+      <img src={image} height={150} alt={product} />
+      <TagList tags={tags} />
+      <ReviewContentWrapper>
+        <Text css="white-space: pre-wrap">{content}</Text>
+        <FavoriteIconWrapper>
+          <SvgIcon variant="favorite" color={favorite ? 'red' : theme.colors.gray4} width={13} height={13} />
+          <Text weight="bold">{favoriteCount}</Text>
+        </FavoriteIconWrapper>
+      </ReviewContentWrapper>
+    </ReviewItemContainer>
+  );
+};
+
+export default ReviewItem;
+
+const ReviewItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+
+  & > img {
+    align-self: center;
+  }
+`;
+
+const ReviewerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ReviewerInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 10px;
+
+  & > img {
+    border-radius: 50%;
+    border: 2px solid ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+const RatingIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: -2px;
+
+  & > svg {
+    padding-bottom: 2px;
+  }
+`;
+
+const ReviewContentWrapper = styled.div``;
+
+const FavoriteIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 4px;
+  margin-top: 8px;
+`;
