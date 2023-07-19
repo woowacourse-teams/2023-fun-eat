cdd6e2798e9e250470aaaf8d7f6eebe0b46c7f25
hae-on <solbi2004@naver.com>
2023-07-19 23:03:04 +0900

    feat: PBProductList구현

---
+++ frontend/src/components/Product/PBProductList/PBProductList.tsx
@@ -0,0 +1,29 @@
+import { styled } from 'styled-components';
+
+import PBProductItem from '../PBProductItem/PBProductItem';
+
+import pbProducts from '@/mocks/data/pbProducts.json';
+
+const PBProductList = () => {
+  return (
+    <PBProductListContainer>
+      {pbProducts.map((pbProduct) => (
+        <li key={pbProduct.id}>
+          <PBProductItem pbProduct={pbProduct} />
+        </li>
+      ))}
+    </PBProductListContainer>
+  );
+};
+
+export default PBProductList;
+
+const PBProductListContainer = styled.ul`
+  display: flex;
+  overflow-x: auto;
+  overflow-y: hidden;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
