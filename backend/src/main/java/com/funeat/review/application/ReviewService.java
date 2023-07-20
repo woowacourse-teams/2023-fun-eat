e04e09bfa4b718eb7dfcb128b686615994936eb4
wugawuga <dnr3655@naver.com>
2023-07-20 13:26:31 +0900

    feat: 리뷰 등록될 때마다 평균 평점 업데이트 구현

--- backend/src/main/java/com/funeat/review/application/ReviewService.java
+++ backend/src/main/java/com/funeat/review/application/ReviewService.java
@@ -19,14 +19,13 @@ import com.funeat.review.presentation.dto.SortingReviewsPageDto;
 import com.funeat.review.presentation.dto.SortingReviewsResponse;
 import com.funeat.tag.domain.Tag;
 import com.funeat.tag.persistence.TagRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -71,6 +70,9 @@ public class ReviewService {
                 .map(findTag -> ReviewTag.createReviewTag(savedReview, findTag))
                 .collect(Collectors.toList());
 
+        final Long countByProduct = reviewRepository.countByProduct(findProduct);
+
+        findProduct.updateAverageRating(savedReview.getRating(), countByProduct);
         reviewTagRepository.saveAll(reviewTags);
         imageService.upload(image);
     }
@@ -79,7 +81,7 @@ public class ReviewService {
     public void likeReview(final Long productId, final Long reviewId, final ReviewFavoriteRequest request) {
         final Member findMember = memberRepository.findById(request.getMemberId())
                 .orElseThrow(IllegalArgumentException::new);
-        final Product findProduct = productRepository.findById(productId)
+        productRepository.findById(productId)
                 .orElseThrow(IllegalArgumentException::new);
         final Review findReview = reviewRepository.findById(reviewId)
                 .orElseThrow(IllegalArgumentException::new);
