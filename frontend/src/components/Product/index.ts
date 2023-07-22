ea5e1ceac8590c88d462ddbc491556582ba36c1c
hae-on <solbi2004@naver.com>
2023-07-22 19:18:37 +0900

    style: 파일명 수정

--- frontend/src/components/Product/index.ts
+++ frontend/src/components/Product/index.ts
@@ -1,7 +1,7 @@
 export { default as ProductDetail } from './ProductDetail/ProductDetail';
 export { default as ProductItem } from './ProductItem/ProductItem';
 export { default as ProductList } from './ProductList/ProductList';
-export { default as ProductRankingItem } from './ProductRankingItem/ProductRankingItem';
+export { default as ProductOverviewItem } from './ProductOverviewItem/ProductOverviewItem';
 export { default as ProductRankingList } from './ProductRankingList/ProductRankingList';
 export { default as ProductTitle } from './ProductTitle/ProductTitle';
 export { default as PBProductList } from './PBProductList/PBProductList';
