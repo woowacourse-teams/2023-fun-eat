69b0dd73fae9128e2e5bf95b98a93ecb3f15b064
TaeeunKim <xodms0309@naver.com>
2023-07-23 17:56:32 +0900

    feat: 태그 더보기 기능 추가

--- frontend/src/components/Review/ReviewTagList/ReviewTagList.tsx
+++ frontend/src/components/Review/ReviewTagList/ReviewTagList.tsx
@@ -1,23 +1,43 @@
 import { Badge, Heading, theme } from '@fun-eat/design-system';
+import { useState } from 'react';
 import styled from 'styled-components';
 
+import { SvgIcon } from '@/components/Common';
 import reviewTagList from '@/mocks/data/reviewTagList.json';
 
 const ReviewTagList = () => {
+  const DISPLAYED_TAGS_LIMIT = 8;
+  const [displayedTagsLimit, setDisplayedTagsLimit] = useState(DISPLAYED_TAGS_LIMIT);
+
+  const showMoreTags = () => {
+    setDisplayedTagsLimit(displayedTagsLimit + DISPLAYED_TAGS_LIMIT);
+  };
+
   return (
     <ReviewTagListContainer>
       <Heading as="h2" size="lg">
         상품에 관한 태그를 선택해주세요 (3개)
       </Heading>
-      <ul>
-        {reviewTagList.map(({ id, content }) => (
+      <TagListWrapper>
+        {reviewTagList.slice(0, displayedTagsLimit).map(({ id, content }) => (
           <TagItem key={id}>
             <Badge color={theme.colors.primary} textColor={theme.textColors.default}>
               {content}
             </Badge>
           </TagItem>
         ))}
-      </ul>
+      </TagListWrapper>
+      {reviewTagList.length > displayedTagsLimit && (
+        <button onClick={showMoreTags}>
+          <SvgIcon
+            variant="arrow"
+            width={15}
+            css={`
+              transform: rotate(270deg);
+            `}
+          />
+        </button>
+      )}
     </ReviewTagListContainer>
   );
 };
@@ -29,6 +49,10 @@ const ReviewTagListContainer = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 20px;
+`;
+
+const TagListWrapper = styled.ul`
+  text-align: center;
 `;
 
 const TagItem = styled.li`
