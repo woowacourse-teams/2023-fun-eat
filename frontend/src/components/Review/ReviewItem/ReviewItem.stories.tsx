7bd89d9695114b1952910a80afda509c3e0654be
TaeeunKim <xodms0309@naver.com>
2023-07-24 21:16:59 +0900

    fix: 스토리북 오류 해결

--- frontend/src/components/Review/ReviewItem/ReviewItem.stories.tsx
+++ frontend/src/components/Review/ReviewItem/ReviewItem.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import ReviewItem from './ReviewItem';
 
-import reviews from '@/mocks/data/reviews.json';
+import mockReviews from '@/mocks/data/reviews.json';
 
 const meta: Meta<typeof ReviewItem> = {
   title: 'review/ReviewItem',
@@ -15,7 +15,7 @@ type Story = StoryObj<typeof ReviewItem>;
 export const Default: Story = {
   render: () => (
     <div style={{ width: '375px', padding: '0 20px' }}>
-      <ReviewItem review={reviews[0]} />
+      <ReviewItem review={mockReviews.reviews[0]} />
     </div>
   ),
 };
@@ -23,7 +23,7 @@ export const Default: Story = {
 export const RebuyAndFavorite: Story = {
   render: () => (
     <div style={{ width: '375px', padding: '0 20px' }}>
-      <ReviewItem review={reviews[1]} />
+      <ReviewItem review={mockReviews.reviews[1]} />
     </div>
   ),
 };
@@ -31,7 +31,7 @@ export const RebuyAndFavorite: Story = {
 export const NoImageReview: Story = {
   render: () => (
     <div style={{ width: '375px', padding: '0 20px' }}>
-      <ReviewItem review={reviews[2]} />
+      <ReviewItem review={mockReviews.reviews[2]} />
     </div>
   ),
 };
