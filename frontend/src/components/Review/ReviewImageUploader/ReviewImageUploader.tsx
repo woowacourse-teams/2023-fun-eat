0c034f68994cb9465d28fc4b2df472f37bdd110b
Taeeun Kim <xodms0309@naver.com>
2023-07-25 15:57:21 +0900

    [FE] feat: 리뷰 사진 업로드 컴포넌트 추가 (#119)
    * feat: 리뷰 이미지 업로드 컴포넌트 추가
    
    * feat: 사진을 업로드 하는 기능 추가
    
    * feat: 사진 장수 업로드 문구 추가
    
    * refactor: 불필요한 styled 삭제
    
    * feat: 이미지 alt 속성 추가
    
    * style: 코드 스타일 정리
    
    * refactor: 인라인 스타일 제거
    
    * feat: 이미지 너비 고정
    
    * style: 코드 스타일 수정

---
+++ frontend/src/components/Review/ReviewImageUploader/ReviewImageUploader.tsx
@@ -0,0 +1,77 @@
+import { Button, Heading, Spacing, Text, useTheme } from '@fun-eat/design-system';
+import type { ChangeEventHandler } from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const ReviewImageUploader = () => {
+  const [reviewImage, setReviewImage] = useState('');
+  const theme = useTheme();
+
+  const uploadReviewImage: ChangeEventHandler<HTMLInputElement> = (event) => {
+    if (!event.target.files) {
+      return;
+    }
+    setReviewImage(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const deleteReviewImage = () => {
+    URL.revokeObjectURL(reviewImage);
+    setReviewImage('');
+  };
+
+  return (
+    <ReviewImageUploaderContainer>
+      <Heading as="h2" size="lg">
+        구매한 상품 사진이 있다면 올려주세요.
+      </Heading>
+      <Text size="sm" color={theme.textColors.disabled}>
+        사진은 1장까지 업로드 가능합니다.
+      </Text>
+      <Spacing size={20} />
+      {reviewImage ? (
+        <ReviewImageButtonWrapper>
+          <img src={reviewImage} alt="업로드한 리뷰 사진" width={200} />
+          <Button variant="filled" color="primary" size="sm" onClick={deleteReviewImage}>
+            삭제하기
+          </Button>
+        </ReviewImageButtonWrapper>
+      ) : (
+        <ImageUploadLabel>
+          +
+          <input type="file" accept="image/*" onChange={uploadReviewImage} />
+        </ImageUploadLabel>
+      )}
+    </ReviewImageUploaderContainer>
+  );
+};
+
+export default ReviewImageUploader;
+
+const ReviewImageUploaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ImageUploadLabel = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 92px;
+  height: 95px;
+  background: ${({ theme }) => theme.colors.gray1};
+  border: 1px solid ${({ theme }) => theme.borderColors.disabled};
+  border-radius: ${({ theme }) => theme.borderRadius.xs};
+  cursor: pointer;
+
+  & > input {
+    display: none;
+  }
+`;
+
+const ReviewImageButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
