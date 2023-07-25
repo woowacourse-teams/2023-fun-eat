6bab4e3cf1779078811ff058f7f3c99c7aa34808
Leejin-Yang <ejin1996@gmail.com>
2023-07-25 20:51:02 +0900

    refactor: BottomSheet 페이지로 이동

--- frontend/src/pages/ProductDetailPage.tsx
+++ frontend/src/pages/ProductDetailPage.tsx
@@ -1,15 +1,25 @@
-import { Spacing } from '@fun-eat/design-system';
+import { BottomSheet, Spacing } from '@fun-eat/design-system';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { SortButton, TabMenu } from '@/components/Common';
+import { SortButton, SortOptionList, TabMenu } from '@/components/Common';
 import { ProductDetailItem, ProductTitle } from '@/components/Product';
 import { ReviewItem } from '@/components/Review';
+import { REVIEW_SORT_OPTIONS } from '@/constants';
+import useBottomSheet from '@/hooks/useBottomSheet';
 import productDetails from '@/mocks/data/productDetails.json';
 import mockReviews from '@/mocks/data/reviews.json';
 
 const ProductDetailPage = () => {
+  const [selectedOption, setSelectedOption] = useState<string>(REVIEW_SORT_OPTIONS[0].label);
+
+  const { ref, handleOpenBottomSheet, handleCloseBottomSheet } = useBottomSheet();
   const { productId } = useParams();
+
+  const selectSortOption = (selectedOptionLabel: string) => {
+    setSelectedOption(selectedOptionLabel);
+  };
 
   // TODO: productId param으로 api 요청 보내면 바뀔 로직
   const targetProductDetail =
@@ -24,11 +34,9 @@ const ProductDetailPage = () => {
       <ProductDetailItem product={targetProductDetail} />
       <Spacing size={36} />
       <TabMenu tabMenus={[`리뷰 ${reviews.length}`, '꿀조합']} />
-      <Spacing size={8} />
       <SortButtonWrapper>
-        <SortButton />
+        <SortButton option={selectedOption} onClick={handleOpenBottomSheet} />
       </SortButtonWrapper>
-      <Spacing size={8} />
       <section>
         {reviews && (
           <ReviewItemWrapper>
@@ -40,6 +48,14 @@ const ProductDetailPage = () => {
           </ReviewItemWrapper>
         )}
       </section>
+      <BottomSheet ref={ref} maxWidth="600px" close={handleCloseBottomSheet}>
+        <SortOptionList
+          options={REVIEW_SORT_OPTIONS}
+          selectedOption={selectedOption}
+          selectSortOption={selectSortOption}
+          close={handleCloseBottomSheet}
+        />
+      </BottomSheet>
     </>
   );
 };
@@ -50,6 +66,7 @@ const SortButtonWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  margin: 20px 0;
 `;
 
 const ReviewItemWrapper = styled.ul`
--- frontend/src/pages/ProductListPage.tsx
+++ frontend/src/pages/ProductListPage.tsx
@@ -1,23 +1,41 @@
-import { Spacing } from '@fun-eat/design-system';
+import { BottomSheet, Spacing } from '@fun-eat/design-system';
+import { useState } from 'react';
 import styled from 'styled-components';
 
-import { CategoryMenu } from '@/components/Common';
-import SortButton from '@/components/Common/SortButton/SortButton';
-import Title from '@/components/Common/Title/Title';
+import { CategoryMenu, SortButton, SortOptionList, Title } from '@/components/Common';
 import { ProductList } from '@/components/Product';
+import { PRODUCT_SORT_OPTIONS } from '@/constants';
+import useBottomSheet from '@/hooks/useBottomSheet';
 import foodCategory from '@/mocks/data/foodCategory.json';
 
 const ProductListPage = () => {
+  const { ref, handleOpenBottomSheet, handleCloseBottomSheet } = useBottomSheet();
+  const [selectedOption, setSelectedOption] = useState<string>(PRODUCT_SORT_OPTIONS[0].label);
+
+  const selectSortOption = (selectedOptionLabel: string) => {
+    setSelectedOption(selectedOptionLabel);
+  };
+
   return (
-    <section>
-      <Title headingTitle="공통 상품" />
-      <Spacing size={30} />
-      <CategoryMenu menuList={foodCategory} menuVariant="food" />
-      <SortButtonWrapper>
-        <SortButton />
-      </SortButtonWrapper>
-      <ProductList />
-    </section>
+    <>
+      <section>
+        <Title headingTitle="공통 상품" />
+        <Spacing size={30} />
+        <CategoryMenu menuList={foodCategory} menuVariant="food" />
+        <SortButtonWrapper>
+          <SortButton option={selectedOption} onClick={handleOpenBottomSheet} />
+        </SortButtonWrapper>
+        <ProductList />
+      </section>
+      <BottomSheet ref={ref} maxWidth="600px" close={handleCloseBottomSheet}>
+        <SortOptionList
+          options={PRODUCT_SORT_OPTIONS}
+          selectedOption={selectedOption}
+          selectSortOption={selectSortOption}
+          close={handleCloseBottomSheet}
+        />
+      </BottomSheet>
+    </>
   );
 };
 
@@ -26,4 +44,5 @@ export default ProductListPage;
 const SortButtonWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
+  margin: 20px 0;
 `;
