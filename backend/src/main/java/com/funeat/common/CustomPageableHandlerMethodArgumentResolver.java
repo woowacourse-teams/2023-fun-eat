d6942e8c778b7168eb03da10ea5145172030d31d
70825 <hello70825@gmail.com>
2023-07-25 16:45:52 +0900

    feat: 페이징 요청이 들어오면 후순위 정렬 조건으로 id 기준 내림차순을 해주는 기능 추가

---
+++ backend/src/main/java/com/funeat/common/CustomPageableHandlerMethodArgumentResolver.java
@@ -0,0 +1,26 @@
+package com.funeat.common;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class CustomPageableHandlerMethodArgumentResolver extends PageableHandlerMethodArgumentResolver {
+
+    @Override
+    public Pageable resolveArgument(MethodParameter methodParameter, ModelAndViewContainer mavContainer,
+                                    NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        final Pageable pageable = super.resolveArgument(methodParameter, mavContainer, webRequest, binderFactory);
+
+        final Sort lastPrioritySort = Sort.by("id").descending();
+
+        return PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
+                pageable.getSort().and(lastPrioritySort));
+    }
+}
