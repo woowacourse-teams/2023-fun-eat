ac2cd1ab4d7ae5d89323a7969ddb90ad25a5da5b
hanueleee <ezzeezz@naver.com>
2023-07-26 16:26:10 +0900

    refactor: 페이징의 totalCount용 countQuery 개선

--- backend/src/main/java/com/funeat/product/persistence/ProductRepository.java
+++ backend/src/main/java/com/funeat/product/persistence/ProductRepository.java
@@ -10,18 +10,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query("SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) " +
+    @Query(value = "SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) " +
             "FROM Product p " +
             "LEFT JOIN p.reviews r " +
             "WHERE p.category = :category " +
-            "GROUP BY p ")
+            "GROUP BY p ",
+            countQuery = "SELECT COUNT(p) FROM Product p WHERE p.category = :category")
     Page<ProductInCategoryDto> findAllByCategory(final @Param("category") Category category, final Pageable pageable);
 
-    @Query("SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) " +
+    @Query(value = "SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) " +
             "FROM Product p " +
             "LEFT JOIN p.reviews r " +
             "WHERE p.category = :category " +
             "GROUP BY p " +
-            "ORDER BY COUNT(r) DESC, p.id DESC ")
+            "ORDER BY COUNT(r) DESC, p.id DESC ",
+            countQuery = "SELECT COUNT(p) FROM Product p WHERE p.category = :category")
     Page<ProductInCategoryDto> findAllByCategoryOrderByReviewCountDesc(final @Param("category") Category category, final Pageable pageable);
 }
