452b8c620c3f45d7fba5e9201de31bd15307042a
Leejin-Yang <ejin1996@gmail.com>
2023-07-26 13:35:41 +0900

    feat: 상품 리뷰 요청 커스텀 훅 작성

--- frontend/src/hooks/product/index.ts
+++ frontend/src/hooks/product/index.ts
@@ -1,1 +1,2 @@
 export { default as useCategory } from './useCategory';
+export { default as useProductReview } from './useProductReview';
---
+++ frontend/src/hooks/product/useProductReview.ts
@@ -0,0 +1,12 @@
+import { useGet } from '../useGet';
+
+import { productApi } from '@/apis';
+import type { ProductReviewResponse } from '@/types/response';
+
+const useProductReview = (productId: string, sort: string) => {
+  return useGet<ProductReviewResponse>(() =>
+    productApi.get({ params: `/${productId}/reviews`, queries: `?sort=${sort}` })
+  );
+};
+
+export default useProductReview;
