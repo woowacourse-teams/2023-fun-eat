48b41c96f4f709a6a3cec206e54b3e10f794b008
Leejin-Yang <ejin1996@gmail.com>
2023-07-27 00:36:23 +0900

    style: body 제네릭 타입 네이밍 통일

--- frontend/src/hooks/review/useReviewFavorite.ts
+++ frontend/src/hooks/review/useReviewFavorite.ts
@@ -2,8 +2,8 @@ import { useMutate } from '../useMutate';
 
 import { productApi } from '@/apis';
 
-const useReviewFavorite = <T>(productId: number, reviewId: number) => {
-  return useMutate((body?: T) => productApi.patch({ params: `/${productId}/reviews/${reviewId}` }, body));
+const useReviewFavorite = <B>(productId: number, reviewId: number) => {
+  return useMutate((body?: B) => productApi.patch({ params: `/${productId}/reviews/${reviewId}` }, body));
 };
 
 export default useReviewFavorite;
