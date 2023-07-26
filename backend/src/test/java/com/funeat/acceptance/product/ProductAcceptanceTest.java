681e2708fe15040e04cd13becbaa90f8b137f935
hanueleee <ezzeezz@naver.com>
2023-07-26 13:46:49 +0900

    test: 카테고리별 상품을 평점이 높은/낮은 순으로 정렬하는 api에 대한 인수 테스트 추가

--- backend/src/test/java/com/funeat/acceptance/product/ProductAcceptanceTest.java
+++ backend/src/test/java/com/funeat/acceptance/product/ProductAcceptanceTest.java
@@ -166,6 +166,66 @@ class ProductAcceptanceTest extends AcceptanceTest {
     }
 
     @Test
+    void 카테고리별_상품_목록을_평점높은순으로_조회한다() {
+        // given
+        final Long categoryId = 카테고리_추가_요청(간편식사);
+        final Product product1 = new Product("삼각김밥1", 1000L, "image.png", "맛있는 삼각김밥1", 5.0, 간편식사);
+        final Product product2 = new Product("삼각김밥2", 2000L, "image.png", "맛있는 삼각김밥2", 3.0, 간편식사);
+        final Product product3 = new Product("삼각김밥3", 1500L, "image.png", "맛있는 삼각김밥3", 1.0, 간편식사);
+        final Product product4 = new Product("삼각김밥4", 1200L, "image.png", "맛있는 삼각김밥4", 4.0, 간편식사);
+        final Product product5 = new Product("삼각김밥5", 2300L, "image.png", "맛있는 삼각김밥5", 4.5, 간편식사);
+        final Product product6 = new Product("삼각김밥6", 1700L, "image.png", "맛있는 삼각김밥6", 2.5, 간편식사);
+        final Product product7 = new Product("삼각김밥7", 1800L, "image.png", "맛있는 삼각김밥7", 2.0, 간편식사);
+        final Product product8 = new Product("삼각김밥8", 800L, "image.png", "맛있는 삼각김밥8", 1.5, 간편식사);
+        final Product product9 = new Product("삼각김밥9", 3100L, "image.png", "맛있는 삼각김밥9", 3.5, 간편식사);
+        final Product product10 = new Product("삼각김밥10", 2700L, "image.png", "맛있는 삼각김밥10", 1.0, 간편식사);
+        final Product product11 = new Product("삼각김밥11", 300L, "image.png", "맛있는 삼각김밥11", 0.5, 간편식사);
+        final List<Product> products = List.of(product1, product2, product3, product4, product5, product6, product7,
+                product8, product9, product10, product11);
+        복수_상품_추가_요청(products);
+
+        // when
+        final var response = 카테고리별_상품_목록_조회_요청(categoryId, "averageRating", "desc", 0);
+
+        // then
+        STATUS_CODE를_검증한다(response, 정상_처리);
+        페이지를_검증한다(response, (long) products.size(), 0L);
+        카테고리별_상품_목록_조회_결과를_검증한다(response,
+                List.of(product1, product5, product4, product9, product2, product6, product7, product8, product3,
+                        product10));
+    }
+
+    @Test
+    void 카테고리별_상품_목록을_평점낮은순으로_조회한다() {
+        // given
+        final Long categoryId = 카테고리_추가_요청(간편식사);
+        final Product product1 = new Product("삼각김밥1", 1000L, "image.png", "맛있는 삼각김밥1", 5.0, 간편식사);
+        final Product product2 = new Product("삼각김밥2", 2000L, "image.png", "맛있는 삼각김밥2", 3.0, 간편식사);
+        final Product product3 = new Product("삼각김밥3", 1500L, "image.png", "맛있는 삼각김밥3", 1.0, 간편식사);
+        final Product product4 = new Product("삼각김밥4", 1200L, "image.png", "맛있는 삼각김밥4", 4.0, 간편식사);
+        final Product product5 = new Product("삼각김밥5", 2300L, "image.png", "맛있는 삼각김밥5", 4.5, 간편식사);
+        final Product product6 = new Product("삼각김밥6", 1700L, "image.png", "맛있는 삼각김밥6", 2.5, 간편식사);
+        final Product product7 = new Product("삼각김밥7", 1800L, "image.png", "맛있는 삼각김밥7", 2.0, 간편식사);
+        final Product product8 = new Product("삼각김밥8", 800L, "image.png", "맛있는 삼각김밥8", 1.5, 간편식사);
+        final Product product9 = new Product("삼각김밥9", 3100L, "image.png", "맛있는 삼각김밥9", 3.5, 간편식사);
+        final Product product10 = new Product("삼각김밥10", 2700L, "image.png", "맛있는 삼각김밥10", 1.0, 간편식사);
+        final Product product11 = new Product("삼각김밥11", 300L, "image.png", "맛있는 삼각김밥11", 0.5, 간편식사);
+        final List<Product> products = List.of(product1, product2, product3, product4, product5, product6, product7,
+                product8, product9, product10, product11);
+        복수_상품_추가_요청(products);
+
+        // when
+        final var response = 카테고리별_상품_목록_조회_요청(categoryId, "averageRating", "asc", 0);
+
+        // then
+        STATUS_CODE를_검증한다(response, 정상_처리);
+        페이지를_검증한다(response, (long) products.size(), 0L);
+        카테고리별_상품_목록_조회_결과를_검증한다(response,
+                List.of(product11, product10, product3, product8, product7, product6, product2, product9, product4,
+                        product5));
+    }
+
+    @Test
     void 상품_상세_정보를_조회한다() {
         // given
         카테고리_추가_요청(간편식사);
