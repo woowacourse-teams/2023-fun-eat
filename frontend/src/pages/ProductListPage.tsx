d7c95de8c66fcfe671b03ce38f0a57a4c1cd10f4
Leejin Yang <ejin1996@gmail.com>
2023-07-26 13:33:56 +0900

    [FE] feat: 정렬 버튼 분리 (#141)
    * chore: 펀잇 디자인 시스템 버전 업데이트
    
    * feat: 리뷰 정렬 옵션 상수 추가 및 정렬 옵션 타입 추가
    
    * refactor: SortButton에서 BottomSheet 제거
    
    * refactor: SortOptionList에서 options prop 추가
    
    * refactor: BottomSheet 페이지로 이동
    
    * style: SortOption -> SortOptionButton으로 수정
    
    * feat: SortButton 스토리에 args 추가
    
    * refactor: useSortOption 커스텀 훅 분리

--- frontend/src/pages/ProductListPage.tsx
+++ frontend/src/pages/ProductListPage.tsx
@@ -1,23 +1,37 @@
-import { Spacing } from '@fun-eat/design-system';
+import { BottomSheet, Spacing } from '@fun-eat/design-system';
 import styled from 'styled-components';
 
-import { CategoryMenu } from '@/components/Common';
-import SortButton from '@/components/Common/SortButton/SortButton';
-import Title from '@/components/Common/Title/Title';
+import { CategoryMenu, SortButton, SortOptionList, Title } from '@/components/Common';
 import { ProductList } from '@/components/Product';
+import { PRODUCT_SORT_OPTIONS } from '@/constants';
+import useBottomSheet from '@/hooks/useBottomSheet';
+import useSortOption from '@/hooks/useSortOption';
 import foodCategory from '@/mocks/data/foodCategory.json';
 
 const ProductListPage = () => {
+  const { ref, handleOpenBottomSheet, handleCloseBottomSheet } = useBottomSheet();
+  const { selectedOption, selectSortOption } = useSortOption(PRODUCT_SORT_OPTIONS[0].label);
+
   return (
-    <section>
-      <Title headingTitle="공통 상품" />
-      <Spacing size={30} />
-      <CategoryMenu menuList={foodCategory} menuVariant="food" />
-      <SortButtonWrapper>
-        <SortButton />
-      </SortButtonWrapper>
-      <ProductList />
-    </section>
+    <>
+      <section>
+        <Title headingTitle="공통 상품" />
+        <Spacing size={30} />
+        <CategoryMenu menuList={foodCategory} menuVariant="food" />
+        <SortButtonWrapper>
+          <SortButton option={selectedOption} onClick={handleOpenBottomSheet} />
+        </SortButtonWrapper>
+        <ProductList />
+      </section>
+      <BottomSheet ref={ref} maxWidth="600px" close={handleCloseBottomSheet}>
+        <SortOptionList
+          options={PRODUCT_SORT_OPTIONS}
+          selectedOption={selectedOption}
+          selectSortOption={selectSortOption}
+          close={handleCloseBottomSheet}
+        />
+      </BottomSheet>
+    </>
   );
 };
 
@@ -26,4 +40,5 @@ export default ProductListPage;
 const SortButtonWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
+  margin: 20px 0;
 `;
