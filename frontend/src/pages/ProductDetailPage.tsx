332a2d927b61389019eafb3dd2d7f887ca6a397d
hae-on <solbi2004@naver.com>
2023-07-27 21:29:12 +0900

    fix: productId type 오류 수정

--- frontend/src/pages/ProductDetailPage.tsx
+++ frontend/src/pages/ProductDetailPage.tsx
@@ -14,19 +14,10 @@ const ProductDetailPage = () => {
   const { ref, isClosing, handleOpenBottomSheet, handleCloseBottomSheet } = useBottomSheet();
   const { selectedOption, selectSortOption } = useSortOption(REVIEW_SORT_OPTIONS[0]);
 
-  if (!productId) {
-    return null;
-  }
+  const { data: productDetail } = useProductDetail(productId as string);
+  const { data: productReviews } = useProductReview(productId as string, selectedOption.value);
 
-  const { data: productDetail } = useProductDetail(productId);
-
-  if (!productDetail) {
-    return null;
-  }
-
-  const { data: productReviews } = useProductReview(productId, selectedOption.value);
-
-  if (!productReviews) {
+  if (!productDetail || !productReviews) {
     return null;
   }
 
