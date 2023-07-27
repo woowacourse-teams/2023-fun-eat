47184333bbf8ef2e51a4ddbc5f502597ecf4d07c
Leejin-Yang <ejin1996@gmail.com>
2023-07-28 03:52:38 +0900

    feat: useLogin 커스텀 훅 구현

---
+++ frontend/src/hooks/auth/useLogin.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+import { authApi } from '@/apis';
+
+const useLogin = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const handleLogin = async (authProvider: string) => {
+    try {
+      const response = await authApi.get({ params: `/${authProvider}` });
+      const locationUrl = response.url;
+
+      if (locationUrl === null) {
+        throw new Error('응답 받은 주소가 없습니다.');
+      }
+
+      window.location.href = locationUrl;
+    } catch (error) {
+      if (!(error instanceof Error)) {
+        return;
+      }
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { handleLogin, isLoading, error };
+};
+
+export default useLogin;
