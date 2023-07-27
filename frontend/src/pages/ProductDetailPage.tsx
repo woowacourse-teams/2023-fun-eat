3a78d00d017823a4342fd1c08d62597bd772d457
hae-on <solbi2004@naver.com>
2023-07-27 12:33:02 +0900

    feat: 리뷰 작성 폼 버튼 연결

--- frontend/src/pages/ProductDetailPage.tsx
+++ frontend/src/pages/ProductDetailPage.tsx
@@ -1,15 +1,17 @@
-import { Spacing } from '@fun-eat/design-system';
+import { BottomSheet, Button, Spacing, useBottomSheet } from '@fun-eat/design-system';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { SortButton, TabMenu } from '@/components/Common';
 import { ProductDetailItem, ProductTitle } from '@/components/Product';
 import { ReviewItem } from '@/components/Review';
+import ReviewRegisterForm from '@/components/Review/ReviewRegisterForm/ReviewRegisterForm';
 import productDetails from '@/mocks/data/productDetails.json';
 import mockReviews from '@/mocks/data/reviews.json';
 
 const ProductDetailPage = () => {
   const { productId } = useParams();
+  const { ref, isClosing, handleOpenBottomSheet, handleCloseBottomSheet } = useBottomSheet();
 
   // TODO: productId param으로 api 요청 보내면 바뀔 로직
   const targetProductDetail =
@@ -40,6 +42,15 @@ const ProductDetailPage = () => {
           </ReviewItemWrapper>
         )}
       </section>
+      <Spacing size={100} />
+      <ReviewRegisterButtonWrapper>
+        <Button type="button" width="100%" height="60px" size="xl" onClick={handleOpenBottomSheet}>
+          리뷰 작성하기
+        </Button>
+      </ReviewRegisterButtonWrapper>
+      <BottomSheet maxWidth="600px" ref={ref} isClosing={isClosing} close={handleCloseBottomSheet}>
+        <ReviewRegisterForm close={handleCloseBottomSheet} />
+      </BottomSheet>
     </>
   );
 };
@@ -56,4 +67,15 @@ const ReviewItemWrapper = styled.ul`
   display: flex;
   flex-direction: column;
   row-gap: 60px;
+`;
+
+const ReviewRegisterButtonWrapper = styled.div`
+  position: fixed;
+  bottom: 60px;
+  left: 50%;
+  width: calc(100% - 40px);
+  max-width: 560px;
+  height: 80px;
+  background: ${({ theme }) => theme.backgroundColors.default};
+  transform: translateX(-50%);
 `;
