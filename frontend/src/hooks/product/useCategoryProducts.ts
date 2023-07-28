c272592364fbe56316d8e69acf5e2c7a7fd93348
TaeeunKim <xodms0309@naver.com>
2023-07-28 17:39:40 +0900

    refactor: product sort option 상수화

--- frontend/src/hooks/product/useCategoryProducts.ts
+++ frontend/src/hooks/product/useCategoryProducts.ts
@@ -1,9 +1,10 @@
 import { useGet } from '../useGet';
 
 import { categoryApi } from '@/apis';
+import { PRODUCT_SORT_OPTIONS } from '@/constants';
 import type { CategoryProductResponse } from '@/types/response';
 
-const useCategoryProducts = (categoryId: number, sort = 'price,desc') => {
+const useCategoryProducts = (categoryId: number, sort: string = PRODUCT_SORT_OPTIONS[0].value) => {
   return useGet<CategoryProductResponse>(
     () => categoryApi.get({ params: `/${categoryId}/products`, queries: `?page=1&sort=${sort}` }),
     [categoryId, sort]
