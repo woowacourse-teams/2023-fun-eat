8a10a912cd172ee4ba8da63a88ca388eba71d148
hae-on <solbi2004@naver.com>
2023-07-28 15:21:38 +0900

    chore: mock data로 수정

--- frontend/src/components/Review/ReviewRegisterForm/ReviewRegisterForm.stories.tsx
+++ frontend/src/components/Review/ReviewRegisterForm/ReviewRegisterForm.stories.tsx
@@ -2,9 +2,14 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import ReviewRegisterForm from './ReviewRegisterForm';
 
+import productDetail from '@/mocks/data/productDetail.json';
+
 const meta: Meta<typeof ReviewRegisterForm> = {
   title: 'review/ReviewRegisterForm',
   component: ReviewRegisterForm,
+  args: {
+    product: productDetail,
+  },
 };
 
 export default meta;
