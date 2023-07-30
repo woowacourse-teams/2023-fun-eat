b4eda663e2b539892cc93f7814eb95f080d40c53
hae-on <solbi2004@naver.com>
2023-07-30 21:19:36 +0900

    feat: useReviewTags index import

--- frontend/src/hooks/review/index.ts
+++ frontend/src/hooks/review/index.ts
@@ -1,2 +1,3 @@
 export { default as useReviewFavorite } from './useReviewFavorite';
 export { default as useDisplayTag } from './useDisplayTag';
+export { default as useReviewTags } from './useReviewTags';
