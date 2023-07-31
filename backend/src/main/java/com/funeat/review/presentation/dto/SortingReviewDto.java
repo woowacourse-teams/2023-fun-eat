0f0f02030a86c454f4e0d9100ced79be8cf98cf5
Dabeen Jeong <hello70825@gmail.com>
2023-07-31 17:37:51 +0900

    [BE] fix: 정렬된 리뷰 목록 페이징 요청시 API가 정상적으로 응답하도록 수정 (#174)

--- backend/src/main/java/com/funeat/review/presentation/dto/SortingReviewDto.java
+++ backend/src/main/java/com/funeat/review/presentation/dto/SortingReviewDto.java
@@ -3,7 +3,7 @@ package com.funeat.review.presentation.dto;
 import com.funeat.member.domain.favorite.ReviewFavorite;
 import com.funeat.review.domain.Review;
 import com.funeat.review.domain.ReviewTag;
-import com.funeat.tag.domain.Tag;
+import com.funeat.tag.dto.TagDto;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,22 +14,15 @@ public class SortingReviewDto {
     private final String profileImage;
     private final String image;
     private final Long rating;
-    private final List<Tag> tags;
+    private final List<TagDto> tags;
     private final String content;
     private final boolean rebuy;
     private final Long favoriteCount;
     private final boolean favorite;
 
-    public SortingReviewDto(final Long id,
-                            final String userName,
-                            final String profileImage,
-                            final String image,
-                            final Long rating,
-                            final List<Tag> tags,
-                            final String content,
-                            final boolean rebuy,
-                            final Long favoriteCount,
-                            final boolean favorite) {
+    public SortingReviewDto(final Long id, final String userName, final String profileImage, final String image,
+                            final Long rating, final List<TagDto> tags, final String content, final boolean rebuy,
+                            final Long favoriteCount, final boolean favorite) {
         this.id = id;
         this.userName = userName;
         this.profileImage = profileImage;
@@ -49,7 +42,7 @@ public class SortingReviewDto {
                 review.getMember().getProfileImage(),
                 review.getImage(),
                 review.getRating(),
-                findTags(review),
+                findTagDtos(review),
                 review.getContent(),
                 review.getReBuy(),
                 review.getFavoriteCount(),
@@ -57,10 +50,10 @@ public class SortingReviewDto {
         );
     }
 
-    private static List<Tag> findTags(final Review review) {
-        return review.getReviewTags()
-                .stream()
+    private static List<TagDto> findTagDtos(final Review review) {
+        return review.getReviewTags().stream()
                 .map(ReviewTag::getTag)
+                .map(TagDto::toDto)
                 .collect(Collectors.toList());
     }
 
@@ -94,7 +87,7 @@ public class SortingReviewDto {
         return rating;
     }
 
-    public List<Tag> getTags() {
+    public List<TagDto> getTags() {
         return tags;
     }
 
