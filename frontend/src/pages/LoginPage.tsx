6d75da82f13dec12092c5ebee8df80cdfc82a8e0
Leejin-Yang <ejin1996@gmail.com>
2023-08-02 02:31:25 +0900

    feat: 로그인 시 로그인 페이지에서 홈으로 리다이렉트 추가

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,18 +1,25 @@
 import { Button, Link, Spacing, Text } from '@fun-eat/design-system';
+import { Navigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Logo from '@/assets/logo.svg';
 import { SvgIcon } from '@/components/Common';
+import useMemberValueContext from '@/hooks/context/useMemberValueContext';
 import useRouteBack from '@/hooks/useRouteBack';
 
 const SLOGAN = '궁금해? 맛있을걸? 먹어봐 🥄';
 const DESCRIPTION = '편의점 음식을 편리하게 찾아보고\n꿀조합 레시피를 공유해보세요.';
 const KAKAO_LOGIN = '카카오 로그인';
 
-const loginLink = process.env.NODE_ENV === 'development' ? '/login/kakao?code=abc' : '/api/auth/kakao';
+const loginLink = process.env.NODE_ENV === 'development' ? '/login/kakao?code=qwe' : '/api/auth/kakao';
 
 const LoginPage = () => {
   const routeBack = useRouteBack();
+  const member = useMemberValueContext();
+
+  if (member !== null) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <LoginPageContainer>
