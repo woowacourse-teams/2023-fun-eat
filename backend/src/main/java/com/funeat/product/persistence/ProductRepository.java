9b6b4b9a733beb5e0d3fefc0e32b7bd0ef52b16a
Go-Jaecheol <gojaech@naver.com>
2023-08-01 18:42:03 +0900

    feat: 전체 상품 중에서 평점이 3보다 큰 상품 및 리뷰 수 조회하는 기능 구현

--- backend/src/main/java/com/funeat/product/persistence/ProductRepository.java
+++ backend/src/main/java/com/funeat/product/persistence/ProductRepository.java
@@ -3,6 +3,8 @@ package com.funeat.product.persistence;
 import com.funeat.product.domain.Category;
 import com.funeat.product.domain.Product;
 import com.funeat.product.dto.ProductInCategoryDto;
+import com.funeat.product.dto.ProductReviewCountDto;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,20 +12,33 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query(value = "SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) " +
-            "FROM Product p " +
-            "LEFT JOIN p.reviews r " +
-            "WHERE p.category = :category " +
-            "GROUP BY p ",
+
+    @Query(value =
+            "SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) "
+                    +
+                    "FROM Product p " +
+                    "LEFT JOIN p.reviews r " +
+                    "WHERE p.category = :category " +
+                    "GROUP BY p ",
             countQuery = "SELECT COUNT(p) FROM Product p WHERE p.category = :category")
     Page<ProductInCategoryDto> findAllByCategory(@Param("category") final Category category, final Pageable pageable);
 
-    @Query(value = "SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) " +
-            "FROM Product p " +
-            "LEFT JOIN p.reviews r " +
-            "WHERE p.category = :category " +
-            "GROUP BY p " +
-            "ORDER BY COUNT(r) DESC, p.id DESC ",
+    @Query(value =
+            "SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) "
+                    +
+                    "FROM Product p " +
+                    "LEFT JOIN p.reviews r " +
+                    "WHERE p.category = :category " +
+                    "GROUP BY p " +
+                    "ORDER BY COUNT(r) DESC, p.id DESC ",
             countQuery = "SELECT COUNT(p) FROM Product p WHERE p.category = :category")
-    Page<ProductInCategoryDto> findAllByCategoryOrderByReviewCountDesc(@Param("category") final Category category, final Pageable pageable);
+    Page<ProductInCategoryDto> findAllByCategoryOrderByReviewCountDesc(@Param("category") final Category category,
+                                                                       final Pageable pageable);
+
+    @Query("SELECT new com.funeat.product.dto.ProductReviewCountDto(p, COUNT(r.id)) "
+            + "FROM Product p "
+            + "LEFT JOIN Review r ON r.product.id = p.id "
+            + "WHERE p.averageRating > 3.0 "
+            + "GROUP BY p.id")
+    List<ProductReviewCountDto> findAllProductsAndReviewCountByAverageRatingGreaterThan3();
 }
