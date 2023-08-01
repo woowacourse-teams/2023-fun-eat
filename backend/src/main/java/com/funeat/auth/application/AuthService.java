2894b9d8a635a3bde33bdace189015efd1363978
Dabeen Jeong <hello70825@gmail.com>
2023-08-01 18:19:09 +0900

    [BE] refactor: readonly 제거 (#203)

--- backend/src/main/java/com/funeat/auth/application/AuthService.java
+++ backend/src/main/java/com/funeat/auth/application/AuthService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 public class AuthService {
 
     private final MemberService memberService;
