4655d04d129b780f784f84db1a1aa2ca92d55186
Leejin-Yang <ejin1996@gmail.com>
2023-08-02 01:59:33 +0900

    feat: 프로필 페이지 내용 추가

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,5 +1,15 @@
+import { Navigate } from 'react-router-dom';
+
+import useMemberValueContext from '@/hooks/context/useMemberValueContext';
+
 const ProfilePage = () => {
-  return <></>;
+  const member = useMemberValueContext();
+
+  if (member === null) {
+    return <Navigate to="/login" />;
+  }
+
+  return <div>{member.nickname}</div>;
 };
 
 export default ProfilePage;
