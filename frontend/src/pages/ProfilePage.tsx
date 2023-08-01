b42952430924b94b87f23b97e2e60788455a1463
Leejin-Yang <ejin1996@gmail.com>
2023-08-02 02:31:54 +0900

    feat: 프로필 페이지에서 사용자 정보 렌더

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,4 +1,6 @@
+import { Text } from '@fun-eat/design-system';
 import { Navigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 import useMemberValueContext from '@/hooks/context/useMemberValueContext';
 
@@ -9,7 +11,27 @@ const ProfilePage = () => {
     return <Navigate to="/login" />;
   }
 
-  return <div>{member.nickname}</div>;
+  return (
+    <ProfilePageContainer>
+      <ProfileImage src={member.profileImage} alt={member.nickname} width={60} height={60} />
+      <Text as="span" size="xl">
+        {member.nickname}
+      </Text>
+    </ProfilePageContainer>
+  );
 };
 
 export default ProfilePage;
+
+const ProfilePageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 20px;
+`;
+
+const ProfileImage = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  border: 1px solid ${({ theme }) => theme.colors.gray4};
+`;
