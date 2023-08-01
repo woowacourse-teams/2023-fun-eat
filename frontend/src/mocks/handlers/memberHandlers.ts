83db003c5aff52918a50d0efb2f84995f7f08958
Leejin-Yang <ejin1996@gmail.com>
2023-08-02 02:31:03 +0900

    feat: 회원 목 이미지 추가

--- frontend/src/mocks/handlers/memberHandlers.ts
+++ frontend/src/mocks/handlers/memberHandlers.ts
@@ -5,11 +5,25 @@ export const memberHandlers = [
     const { mockSessionId } = req.cookies;
 
     if (mockSessionId === 'abc123') {
-      return res(ctx.status(200), ctx.json({ nickname: '냐미', profileImage: '' }));
+      return res(
+        ctx.status(200),
+        ctx.json({
+          nickname: '냐미',
+          profileImage:
+            'https://github.com/woowacourse-teams/2023-fun-eat/assets/78616893/991f7b69-53bf-4d03-96e1-988c34d010ed',
+        })
+      );
     }
 
     if (mockSessionId === 'qwe456') {
-      return res(ctx.status(200), ctx.json({ nickname: '뇨미', profileImage: '' }));
+      return res(
+        ctx.status(200),
+        ctx.json({
+          nickname: '뇨미',
+          profileImage:
+            'https://github.com/woowacourse-teams/2023-fun-eat/assets/78616893/84a20dae-4770-4497-ae25-2dd552261aab',
+        })
+      );
     }
 
     return res(ctx.status(403));
