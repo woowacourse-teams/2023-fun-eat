8f538714aba44e9dc72776e7ab216973b9d633ad
Leejin-Yang <ejin1996@gmail.com>
2023-08-01 22:13:16 +0900

    feat: 요청 옵션에 credentials 추가

--- frontend/src/apis/ApiClient.ts
+++ frontend/src/apis/ApiClient.ts
@@ -3,6 +3,7 @@ import { fetchApi } from './fetch';
 interface RequestOptions {
   params?: string;
   queries?: string;
+  credentials?: boolean;
 }
 
 export class ApiClient {
@@ -19,34 +20,38 @@ export class ApiClient {
     return '/api' + this.#path + params + queries;
   }
 
-  get({ params, queries }: RequestOptions) {
+  get({ params, queries, credentials = false }: RequestOptions) {
     return fetchApi(this.getUrl(params, queries), {
       method: 'GET',
       headers: this.#headers,
+      credentials: credentials ? 'include' : 'omit',
     });
   }
 
-  post<B>({ params, queries }: RequestOptions, body?: B) {
+  post<B>({ params, queries, credentials = false }: RequestOptions, body?: B) {
     return fetchApi(this.getUrl(params, queries), {
       method: 'POST',
       headers: this.#headers,
       body: body ? JSON.stringify(body) : null,
+      credentials: credentials ? 'include' : 'omit',
     });
   }
 
-  patch<B>({ params, queries }: RequestOptions, body?: B) {
+  patch<B>({ params, queries, credentials = false }: RequestOptions, body?: B) {
     return fetchApi(this.getUrl(params, queries), {
       method: 'PATCH',
       headers: this.#headers,
       body: body ? JSON.stringify(body) : null,
+      credentials: credentials ? 'include' : 'omit',
     });
   }
 
-  delete<B>({ params, queries }: RequestOptions, body?: B) {
+  delete<B>({ params, queries, credentials = false }: RequestOptions, body?: B) {
     return fetchApi(this.getUrl(params, queries), {
       method: 'DELETE',
       headers: this.#headers,
       body: body ? JSON.stringify(body) : null,
+      credentials: credentials ? 'include' : 'omit',
     });
   }
 }
