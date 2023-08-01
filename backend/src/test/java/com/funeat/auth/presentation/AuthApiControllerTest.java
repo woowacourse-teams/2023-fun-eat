9d7a862876006212c8f441bc9a1304f17b06f2b2
70825 <hello70825@gmail.com>
2023-08-01 13:35:44 +0900

    feat: 로그인 Swagger UI 추가

--- backend/src/test/java/com/funeat/auth/presentation/AuthApiControllerTest.java
+++ backend/src/test/java/com/funeat/auth/presentation/AuthApiControllerTest.java
@@ -15,9 +15,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(AuthController.class)
+@WebMvcTest(AuthApiController.class)
 @SuppressWarnings("NonAsciiCharacters")
-public class AuthControllerTest {
+public class AuthApiControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
