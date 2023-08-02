258f165ad741c119819c3181fbff67cc8851e27c
wugawuga <dnr3655@naver.com>
2023-08-02 10:49:02 +0900

    refactor: JsonCreator 추가

--- backend/src/main/java/com/funeat/review/presentation/dto/ReviewFavoriteRequest.java
+++ backend/src/main/java/com/funeat/review/presentation/dto/ReviewFavoriteRequest.java
@@ -1,10 +1,14 @@
 package com.funeat.review.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ReviewFavoriteRequest {
 
     private final Boolean favorite;
 
-    public ReviewFavoriteRequest(final Boolean favorite) {
+    @JsonCreator
+    public ReviewFavoriteRequest(@JsonProperty("favorite") final Boolean favorite) {
         this.favorite = favorite;
     }
 
