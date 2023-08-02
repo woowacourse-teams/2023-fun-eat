bcab070285670c90d2f606f9347e880f7aff0ec1
JFe <33208246+Go-Jaecheol@users.noreply.github.com>
2023-08-02 19:59:11 +0900

    [BE] fix: CORS 관련 설정 제거 (#238)

--- backend/src/main/java/com/funeat/common/WebConfig.java
+++ backend/src/main/java/com/funeat/common/WebConfig.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -46,15 +45,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(customPageableHandlerMethodArgumentResolver);
         resolvers.add(authArgumentResolver);
-    }
-
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://localhost:80", "https://localhost:443")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .exposedHeaders("Location", "Set-Cookie")
-                .allowCredentials(true);
     }
 }
