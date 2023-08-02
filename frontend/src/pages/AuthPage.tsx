fb7c370c22fc87bf06d85ca7f3b8d5da135d4b46
Leejin Yang <ejin1996@gmail.com>
2023-08-02 14:53:20 +0900

    [FE] refactor: 세션아이디 요청 메서드 GET으로 수정 (#222)

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,31 +1,34 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 
-import { memberApi } from '@/apis';
-import { useAuth } from '@/hooks/auth';
+import { loginApi, memberApi } from '@/apis';
 import { useMemberActionContext } from '@/hooks/context';
 import type { Member } from '@/types/member';
 
 const AuthPage = () => {
-  const { provider } = useParams();
+  const { authProvider } = useParams();
   const [searchParams] = useSearchParams();
   const code = searchParams.get('code');
   const navigate = useNavigate();
 
   const { handleNewMember } = useMemberActionContext();
-  const { request } = useAuth(provider ?? 'kakao', code ?? 'code');
 
-  const locationRef = useRef<string>('');
+  const [location, setLocation] = useState('');
 
   const getSessionId = async () => {
-    const response = await request();
+    const response = await loginApi.get({ params: `/oauth2/code/${authProvider}`, queries: `?code=${code}` });
 
     if (!response) {
       throw new Error('로그인에 실패했습니다.');
     }
 
     const location = response.headers.get('Location');
-    locationRef.current = location ?? '';
+
+    if (location === null) {
+      throw new Error('Location이 없습니다.');
+    }
+
+    setLocation(location);
   };
 
   const getMember = async () => {
@@ -33,7 +36,7 @@ const AuthPage = () => {
     const member: Member = await response.json();
 
     handleNewMember(member);
-    navigate(locationRef.current, { replace: true });
+    navigate(location, { replace: true });
   };
 
   useEffect(() => {
@@ -45,12 +48,12 @@ const AuthPage = () => {
   }, []);
 
   useEffect(() => {
-    if (locationRef.current === '') {
+    if (location === '') {
       return;
     }
 
     getMember();
-  }, [locationRef.current]);
+  }, [location]);
 
   return <></>;
 };
