3ccf685bff4a519b5f9bfc2d0bb52304b19e5e22
Leejin Yang <ejin1996@gmail.com>
2023-08-03 01:50:25 +0900

    [FE] feat: 리뷰 좋아요 요청 수정 (#268)

--- frontend/src/hooks/review/useReviewFavorite.ts
+++ frontend/src/hooks/review/useReviewFavorite.ts
@@ -3,7 +3,9 @@ import { useMutate } from '../useMutate';
 import { productApi } from '@/apis';
 
 const useReviewFavorite = <B>(productId: number, reviewId: number) => {
-  return useMutate((body?: B) => productApi.patch({ params: `/${productId}/reviews/${reviewId}` }, body));
+  return useMutate((body?: B) =>
+    productApi.patch({ params: `/${productId}/reviews/${reviewId}`, credentials: true }, body)
+  );
 };
 
 export default useReviewFavorite;
