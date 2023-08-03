9816d487feecba5350a4911542e67fd60ac92949
Leejin-Yang <ejin1996@gmail.com>
2023-08-03 15:23:09 +0900

    feat: 비로그인 리뷰 조회 처리

--- frontend/src/components/Review/ReviewList/ReviewList.tsx
+++ frontend/src/components/Review/ReviewList/ReviewList.tsx
@@ -1,3 +1,4 @@
+import { Text } from '@fun-eat/design-system';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -6,13 +7,24 @@ import ReviewItem from '../ReviewItem/ReviewItem';
 import useInfiniteProductReviews from '@/hooks/product/useInfiniteProductReviews';
 import type { SortOption } from '@/types/common';
 
+const LOGIN_ERROR_MESSAGE =
+  '로그인 해야 상품 리뷰를 볼 수 있어요.\n펀잇에 가입하고 편의점 상품의 리뷰를 확인해보세요 😊';
+
 interface ReviewListProps {
   productId: number;
   selectedOption: SortOption;
 }
 
 const ReviewList = ({ productId, selectedOption }: ReviewListProps) => {
-  const { productReviews, scrollRef } = useInfiniteProductReviews(productId, selectedOption.value);
+  const { productReviews, scrollRef, error } = useInfiniteProductReviews(productId, selectedOption.value);
+
+  if (error) {
+    return (
+      <ErrorDescription align="center" weight="bold" size="lg">
+        {LOGIN_ERROR_MESSAGE}
+      </ErrorDescription>
+    );
+  }
 
   return (
     <>
@@ -34,4 +46,10 @@ const ReviewListContainer = styled.ul`
   display: flex;
   flex-direction: column;
   row-gap: 60px;
+`;
+
+const ErrorDescription = styled(Text)`
+  padding: 40px 0;
+  white-space: pre-line;
+  word-break: break-all;
 `;
