298dc70465a92b1402d11e4bab698a3d5a7bf645
sᴏʟʙɪ ☔️ <solbi2004@naver.com>
2023-08-03 17:04:46 +0900

    [FE] feat: 리뷰 작성 필수값 미입력시 작성 버튼 비활성화 (#287)
    * chore: 주석 처리 풀기
    
    * feat: 미입력시 버튼 문구 추가

--- frontend/src/components/Review/ReviewRegisterForm/ReviewRegisterForm.tsx
+++ frontend/src/components/Review/ReviewRegisterForm/ReviewRegisterForm.tsx
@@ -18,7 +18,7 @@ import type { ProductDetail } from '@/types/product';
 
 const MIN_RATING_SCORE = 0;
 const MIN_SELECTED_TAGS_COUNT = 1;
-const MIN_CONTENT_LENGTH = 1;
+const MIN_CONTENT_LENGTH = 0;
 
 interface ReviewRegisterFormProps {
   product: ProductDetail;
@@ -77,13 +77,13 @@ const ReviewRegisterForm = ({ product, close }: ReviewRegisterFormProps) => {
     console.log(response, '리뷰가 성공적으로 등록되었습니다.');
   };
 
-  // useEffect(() => {
-  //   const isValid =
-  //     rating > MIN_RATING_SCORE &&
-  //     selectedTags.length === MIN_SELECTED_TAGS_COUNT &&
-  //     content.length > MIN_CONTENT_LENGTH;
-  //   setSubmitEnabled(isValid);
-  // }, [rating, selectedTags, content]);
+  useEffect(() => {
+    const isValid =
+      rating > MIN_RATING_SCORE &&
+      selectedTags.length === MIN_SELECTED_TAGS_COUNT &&
+      content.length > MIN_CONTENT_LENGTH;
+    setSubmitEnabled(isValid);
+  }, [rating, selectedTags, content]);
 
   return (
     <ReviewRegisterFormContainer>
@@ -120,9 +120,9 @@ const ReviewRegisterForm = ({ product, close }: ReviewRegisterFormProps) => {
           customHeight="60px"
           size="xl"
           weight="bold"
-          // disabled={!submitEnabled}
+          disabled={!submitEnabled}
         >
-          등록하기
+          {submitEnabled ? '리뷰 등록하기' : '꼭 입력해야 하는 항목이 있어요'}
         </FormButton>
       </RegisterForm>
     </ReviewRegisterFormContainer>
@@ -158,7 +158,7 @@ const RegisterForm = styled.form`
 `;
 
 const FormButton = styled(Button)`
-  background: ${({ theme, disabled }) => (disabled ? theme.colors.grey : theme.colors.primary)};
+  background: ${({ theme, disabled }) => (disabled ? theme.colors.gray3 : theme.colors.primary)};
   color: ${({ theme, disabled }) => (disabled ? theme.colors.white : theme.colors.black)};
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
 `;
