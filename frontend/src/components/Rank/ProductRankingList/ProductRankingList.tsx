8f82cedb9b87e157b3f8115cb23d46b9af01765e
Taeeun Kim <xodms0309@naver.com>
2023-08-03 19:37:34 +0900

    [FE] feat: 상품 랭킹 API 연결 (#299)
    * refactor: api 명세가 바뀜에 따라 데이터 구조 변경
    
    * feat: mock product ranking get 요청 handler 추가
    
    * feat: useProductRanking hook 작성
    
    * feat: 상품 랭킹 api 연결
    
    * chore: storybook 오류로 인해 yarn 재실행
    
    * feat: 스토리북에서 빠진 props 추가
    
    * refactor: productRanking 네이밍 단수에서 복수로 변경
    
    * refactor: ProductRankingResponse 분리
    
    * refactor: idx->index로 네이밍 수정

--- frontend/src/components/Rank/ProductRankingList/ProductRankingList.tsx
+++ frontend/src/components/Rank/ProductRankingList/ProductRankingList.tsx
@@ -1,14 +1,18 @@
 import { Spacing } from '@fun-eat/design-system';
 
 import { ProductOverviewItem } from '@/components/Product';
-import productRanking from '@/mocks/data/productRanking.json';
+import type { ProductRanking } from '@/types/ranking';
 
-const ProductRankingList = () => {
+interface ProductRankingListProps {
+  productRankings: ProductRanking[];
+}
+
+const ProductRankingList = ({ productRankings }: ProductRankingListProps) => {
   return (
     <ul>
-      {productRanking.map((productRanking) => (
-        <li key={productRanking.id}>
-          <ProductOverviewItem rank={productRanking.rank} name={productRanking.name} image={productRanking.image} />
+      {productRankings.map(({ id, name, image }, index) => (
+        <li key={id}>
+          <ProductOverviewItem rank={index + 1} name={name} image={image} />
           <Spacing size={16} />
         </li>
       ))}
