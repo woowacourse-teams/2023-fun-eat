0d12842d51a687df76af9b713f819a9c586c2858
TaeeunKim <xodms0309@naver.com>
2023-08-03 17:00:50 +0900

    feat: mock product ranking get 요청 handler 추가

--- frontend/src/mocks/handlers/rankingHandlers.ts
+++ frontend/src/mocks/handlers/rankingHandlers.ts
@@ -1,8 +1,12 @@
 import { rest } from 'msw';
 
+import mockProductRankingList from '../data/productRankingList.json';
 import mockReviewRankingList from '../data/reviewRankingList.json';
 
 export const rankingHandlers = [
+  rest.get('/api/ranks/products', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockProductRankingList));
+  }),
   rest.get('/api/ranks/reviews', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockReviewRankingList));
   }),
