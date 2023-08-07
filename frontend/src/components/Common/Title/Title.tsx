e8b853a313bbdc831c9a2ed5ce557bf3d86ef69e
TaeeunKim <xodms0309@naver.com>
2023-08-07 15:24:04 +0900

    refactor: 홈으로 가는 기능을 버튼에서 링크 태그로 변경

--- frontend/src/components/Common/Title/Title.tsx
+++ frontend/src/components/Common/Title/Title.tsx
@@ -1,10 +1,10 @@
-import { Button, Link, Text, theme } from '@fun-eat/design-system';
+import { Link, Text, theme } from '@fun-eat/design-system';
 import { Link as RouterLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 import SvgIcon from '../Svg/SvgIcon';
 
-import useRoutePage from '@/hooks/useRoutePage';
+import { PATH } from '@/constants/path';
 
 interface TitleProps {
   headingTitle: string;
@@ -12,16 +12,12 @@ interface TitleProps {
 }
 
 const Title = ({ headingTitle, routeDestination }: TitleProps) => {
-  const { routeHome } = useRoutePage();
-
   return (
     <TitleContainer>
-      <Button type="button" variant="transparent" onClick={routeHome} aria-label="홈 화면으로 가기">
-        <SvgIconWrapper>
-          <SvgIcon variant="arrow" color={theme.colors.gray5} width={20} height={20} />
-        </SvgIconWrapper>
-      </Button>
-      <TitleLink as={RouterLink} to={routeDestination}>
+      <HomeLink as={RouterLink} to={PATH.HOME}>
+        <SvgIcon variant="arrow" color={theme.colors.gray5} width={20} height={20} />
+      </HomeLink>
+      <TitleLink as={RouterLink} to={routeDestination} replace>
         <Text as="span" size="xl" weight="bold">
           {headingTitle}
         </Text>
@@ -40,7 +36,7 @@ const TitleContainer = styled.div`
   position: relative;
 `;
 
-const SvgIconWrapper = styled.span`
+const HomeLink = styled(Link)`
   position: absolute;
   top: 8px;
   left: 0;
