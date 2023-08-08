abb88326cfb37fa4e781390f02dbb273ca5c7449
70825 <hello70825@gmail.com>
2023-08-08 17:46:46 +0900

    test: ProductTest 추가 성공 테스트 및 실패 테스트 작성

--- backend/src/test/java/com/funeat/product/domain/ProductTest.java
+++ backend/src/test/java/com/funeat/product/domain/ProductTest.java
@@ -1,6 +1,7 @@
 package com.funeat.product.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -15,48 +16,128 @@ class ProductTest {
     @Nested
     class updateAverageRating_테스트 {
 
-        @Test
-        void 평균_평점_업데이트를_할_수_있다() {
-            // given
-            final var product = new Product("testName", 1000L, "testImage", "testContent", null);
-            final var reviewRating1 = 4L;
-            final var reviewRating2 = 2L;
-            final var reviewCount1 = 1L;
-            final var reviewCount2 = 2L;
+        @Nested
+        class 성공_테스트 {
 
-            final var expected1 = 4.0;
-            final var expected2 = 3.0;
+            @Test
+            void 평균_평점을_업데이트_할_수_있다() {
+                // given
+                final var product = new Product("testName", 1000L, "testImage", "testContent", null);
+                final var reviewRating = 4L;
+                final var reviewCount = 1L;
 
-            // when
-            product.updateAverageRating(reviewRating1, reviewCount1);
-            final var actual1 = product.getAverageRating();
+                final var expected = 4.0;
 
-            product.updateAverageRating(reviewRating2, reviewCount2);
-            final var actual2 = product.getAverageRating();
+                // when
+                product.updateAverageRating(reviewRating, reviewCount);
+                final var actual = product.getAverageRating();
 
-            assertSoftly(softAssertions -> {
-                softAssertions.assertThat(actual1).isEqualTo(expected1);
-                softAssertions.assertThat(actual2).isEqualTo(expected2);
-            });
+                // then
+                assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            void 평균_평점을_여러번_업데이트_할_수_있다() {
+                // given
+                final var product = new Product("testName", 1000L, "testImage", "testContent", null);
+                final var reviewRating1 = 4L;
+                final var reviewRating2 = 2L;
+                final var reviewCount1 = 1L;
+                final var reviewCount2 = 2L;
+
+                final var expected1 = 4.0;
+                final var expected2 = 3.0;
+
+                // when
+                product.updateAverageRating(reviewRating1, reviewCount1);
+                final var actual1 = product.getAverageRating();
+
+                product.updateAverageRating(reviewRating2, reviewCount2);
+                final var actual2 = product.getAverageRating();
+
+                // then
+                assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(actual1).isEqualTo(expected1);
+                    softAssertions.assertThat(actual2).isEqualTo(expected2);
+                });
+            }
+        }
+
+        @Nested
+        class 실패_테스트 {
+
+            @Test
+            void 리뷰_평점에_null_값이_들어오면_예외가_발생한다() {
+                // given
+                final var product = new Product("testName", 1000L, "testImage", "testContent", null);
+                final var reviewCount = 1L;
+
+                // when
+                assertThatThrownBy(() -> product.updateAverageRating(null, reviewCount))
+                        .isInstanceOf(NullPointerException.class);
+            }
+
+            @Test
+            void 리뷰_평점이_0점이라면_예외가_발생해야하는데_관련_로직이_없어_통과하고_있다() {
+                // given
+                final var product = new Product("testName", 1000L, "testImage", "testContent", null);
+                final var reviewRating = 0L;
+                final var reviewCount = 1L;
+
+                // when
+                product.updateAverageRating(reviewRating, reviewCount);
+            }
+
+            @Test
+            void 리뷰_개수가_0개라면_예외가_발생해야하는데_calculatedRating값이_infinity가_나와_통과하고_있다() {
+                // given
+                final var product = new Product("testName", 1000L, "testImage", "testContent", null);
+                final var reviewRating = 3L;
+                final var reviewCount = 0L;
+
+                // when
+                product.updateAverageRating(reviewRating, reviewCount);
+            }
         }
     }
 
     @Nested
     class calculateRankingScore_테스트 {
 
-        @Test
-        void 평균_평점과_리뷰_수로_해당_상품의_랭킹_점수를_구할_수_있다() {
-            // given
-            final var product = new Product("testName", 1000L, "testImage", "testContent", 4.0, null);
-            final var reviewCount = 9L;
+        @Nested
+        class 성공_테스트 {
 
-            final var expected = 3.5;
+            @Test
+            void 평균_평점과_리뷰_수로_해당_상품의_랭킹_점수를_구할_수_있다() {
+                // given
+                final var product = new Product("testName", 1000L, "testImage", "testContent", 4.0, null);
+                final var reviewCount = 9L;
 
-            // when
-            final var rankingScore = product.calculateRankingScore(reviewCount);
+                final var expected = 3.5;
 
-            // then
-            assertThat(rankingScore).isEqualTo(expected);
+                // when
+                final var rankingScore = product.calculateRankingScore(reviewCount);
+
+                // then
+                assertThat(rankingScore).isEqualTo(expected);
+            }
+        }
+
+        @Nested
+        class 실패_테스트 {
+
+            @Test
+            void 리뷰_수가_마이너스_일로_나온다면_무한대로_나와서_예외가_나와야하는데_통과하고_있다() {
+                // given
+                final var product = new Product("testName", 1000L, "testImage", "testContent", 4.0, null);
+                final var reviewCount = -1L;
+
+                // when
+                final var rankingScore = product.calculateRankingScore(reviewCount);
+
+                // then
+                assertThat(rankingScore).isInfinite();
+            }
         }
     }
 }
