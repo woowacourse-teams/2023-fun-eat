b1f66add48f99cba46ad15ecce26b651fed55812
70825 <hello70825@gmail.com>
2023-08-08 13:27:36 +0900

    test: MemberServiceTest에 실패 테스트 추가

--- backend/src/test/java/com/funeat/member/application/MemberServiceTest.java
+++ backend/src/test/java/com/funeat/member/application/MemberServiceTest.java
@@ -18,192 +18,274 @@ class MemberServiceTest extends ServiceTest {
     @Nested
     class findOrCreateMember_테스트 {
 
-        @Test
-        void 이미_가입된_사용자면_가입하지_않고_반환한다() {
-            // given
-            final var member = new Member("test", "www.test.com", "1");
-            단일_멤버_저장(member);
+        @Nested
+        class 성공_테스트 {
 
-            final var userInfoDto = new UserInfoDto(1L, "test", "www.test.com");
+            @Test
+            void 이미_가입된_사용자면_가입하지_않고_반환한다() {
+                // given
+                final var member = new Member("test", "www.test.com", "1");
+                단일_멤버_저장(member);
 
-            final var expected = memberRepository.findAll();
+                final var userInfoDto = new UserInfoDto(1L, "test", "www.test.com");
 
-            // when
-            final var actual = memberService.findOrCreateMember(userInfoDto);
+                final var expected = memberRepository.findAll();
 
-            // then
-            assertSoftly(softAssertions -> {
-                softAssertions.assertThat(actual.isSignUp()).isFalse();
-                softAssertions.assertThat(expected).containsExactly(actual.getMember());
-            });
+                // when
+                final var actual = memberService.findOrCreateMember(userInfoDto);
+
+                // then
+                assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(actual.isSignUp()).isFalse();
+                    softAssertions.assertThat(expected).containsExactly(actual.getMember());
+                });
+            }
+
+            @Test
+            void 가입되지_않은_사용자면_가입하고_반환하다() {
+                // given
+                final var member = new Member("test", "www.test.com", "1");
+                단일_멤버_저장(member);
+
+                final var userInfoDto = new UserInfoDto(2L, "test", "www.test.com");
+
+                final var expected = memberRepository.findAll();
+
+                // when
+                final var actual = memberService.findOrCreateMember(userInfoDto);
+
+                // then
+                assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(actual.isSignUp()).isTrue();
+                    softAssertions.assertThat(expected).doesNotContain(actual.getMember());
+                });
+            }
         }
 
-        @Test
-        void 가입되지_않은_사용자면_가입하고_반환하다() {
-            // given
-            final var member = new Member("test", "www.test.com", "1");
-            단일_멤버_저장(member);
+        @Nested
+        class 실패_테스트 {
 
-            final var userInfoDto = new UserInfoDto(2L, "test", "www.test.com");
+            @Test
+            void platformId가_null이면_예외가_발생한다() {
+                // given
+                final var userInfoDto = new UserInfoDto(null, "test", "www.test.com");
 
-            final var expected = memberRepository.findAll();
-
-            // when
-            final var actual = memberService.findOrCreateMember(userInfoDto);
-
-            // then
-            assertSoftly(softAssertions -> {
-                softAssertions.assertThat(actual.isSignUp()).isTrue();
-                softAssertions.assertThat(expected).doesNotContain(actual.getMember());
-            });
+                // when, then
+                assertThatThrownBy(() -> memberService.findOrCreateMember(userInfoDto))
+                        .isInstanceOf(NullPointerException.class);
+            }
         }
     }
 
     @Nested
     class getMemberProfile_테스트 {
 
-        @Test
-        void 사용자_정보를_확인하다() {
-            // given
-            final var member = new Member("test", "http://www.test.com", "1");
-            final var memberId = 단일_멤버_저장(member);
+        @Nested
+        class 성공_테스트 {
 
-            final var expected = MemberProfileResponse.toResponse(member);
+            @Test
+            void 사용자_정보를_확인하다() {
+                // given
+                final var member = new Member("test", "http://www.test.com", "1");
+                final var memberId = 단일_멤버_저장(member);
 
-            // when
-            final var actual = memberService.getMemberProfile(memberId);
+                final var expected = MemberProfileResponse.toResponse(member);
 
-            // then
-            assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+                // when
+                final var actual = memberService.getMemberProfile(memberId);
+
+                // then
+                assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+            }
         }
 
-        @Test
-        void 존재하지_않는_사용자_정보를_조회하면_예외가_발생한다() {
-            // given
-            final var member = new Member("test", "http://www.test.com", "1");
-            final var wrongMemberId = 단일_멤버_저장(member) + 1L;
+        @Nested
+        class 실패_테스트 {
 
-            // when, then
-            assertThatThrownBy(() -> memberService.getMemberProfile(wrongMemberId))
-                    .isInstanceOf(IllegalArgumentException.class);
+            @Test
+            void 존재하지_않는_사용자_정보를_조회하면_예외가_발생한다() {
+                // given
+                final var member = new Member("test", "http://www.test.com", "1");
+                final var wrongMemberId = 단일_멤버_저장(member) + 1L;
+
+                // when, then
+                assertThatThrownBy(() -> memberService.getMemberProfile(wrongMemberId))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
         }
     }
 
     @Nested
     class modify_테스트 {
 
-        @Test
-        void 닉네임과_프로필_사진이_그대로면_사용자_정보는_바뀌지_않는다() {
-            // given
-            final var nickname = "test";
-            final var profileImage = "http://www.test.com";
-            final var request = new MemberRequest(nickname, profileImage);
+        @Nested
+        class 성공_테스트 {
 
-            final var member = new Member(nickname, profileImage, "1");
-            final var memberId = 단일_멤버_저장(member);
+            @Test
+            void 닉네임과_프로필_사진이_그대로면_사용자_정보는_바뀌지_않는다() {
+                // given
+                final var nickname = "test";
+                final var profileImage = "http://www.test.com";
+                final var request = new MemberRequest(nickname, profileImage);
 
-            final var expected = memberRepository.findById(memberId).get();
-            final var expectedNickname = expected.getNickname();
-            final var expectedProfileImage = expected.getProfileImage();
+                final var member = new Member(nickname, profileImage, "1");
+                final var memberId = 단일_멤버_저장(member);
 
-            // when
-            memberService.modify(memberId, request);
-            final var actual = memberRepository.findById(memberId).get();
-            final var actualNickname = actual.getNickname();
-            final var actualProfileImage = actual.getProfileImage();
+                final var expected = memberRepository.findById(memberId).get();
+                final var expectedNickname = expected.getNickname();
+                final var expectedProfileImage = expected.getProfileImage();
 
-            // then
-            assertSoftly(softAssertions -> {
-                softAssertions.assertThat(actualNickname).isEqualTo(expectedNickname);
-                softAssertions.assertThat(actualProfileImage).isEqualTo(expectedProfileImage);
-            });
+                // when
+                memberService.modify(memberId, request);
+                final var actual = memberRepository.findById(memberId).get();
+                final var actualNickname = actual.getNickname();
+                final var actualProfileImage = actual.getProfileImage();
+
+                // then
+                assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(actualNickname).isEqualTo(expectedNickname);
+                    softAssertions.assertThat(actualProfileImage).isEqualTo(expectedProfileImage);
+                });
+            }
+
+            @Test
+            void 닉네임만_바뀌고_프로필_사진은_그대로면_닉네임만_바뀐다() {
+                // given
+                final var beforeNickname = "before";
+                final var profileImage = "http://www.test.com";
+                final var afterNickname = "after";
+                final var request = new MemberRequest(afterNickname, profileImage);
+
+                final var member = new Member(beforeNickname, profileImage, "1");
+                final var memberId = 단일_멤버_저장(member);
+
+                final var expected = memberRepository.findById(memberId).get();
+                final var expectedNickname = expected.getNickname();
+                final var expectedProfileImage = expected.getProfileImage();
+
+                // when
+                memberService.modify(memberId, request);
+                final var actual = memberRepository.findById(memberId).get();
+                final var actualNickname = actual.getNickname();
+                final var actualProfileImage = actual.getProfileImage();
+
+                // then
+                assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(actualNickname).isNotEqualTo(expectedNickname);
+                    softAssertions.assertThat(actualProfileImage).isEqualTo(expectedProfileImage);
+                });
+            }
+
+            @Test
+            void 닉네임은_그대로이고_프로필_사진이_바뀌면_프로필_사진만_바뀐다() {
+                // given
+                final var nickname = "test";
+                final var beforeProfileImage = "http://www.before.com";
+                final var afterProfileImage = "http://www.after.com";
+
+                final var request = new MemberRequest(nickname, afterProfileImage);
+
+                final var member = new Member(nickname, beforeProfileImage, "1");
+                final var memberId = 단일_멤버_저장(member);
+
+                final var expected = memberRepository.findById(memberId).get();
+                final var expectedNickname = expected.getNickname();
+                final var expectedProfileImage = expected.getProfileImage();
+
+                // when
+                memberService.modify(memberId, request);
+                final var actual = memberRepository.findById(memberId).get();
+                final var actualNickname = actual.getNickname();
+                final var actualProfileImage = actual.getProfileImage();
+
+                // then
+                assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(actualNickname).isEqualTo(expectedNickname);
+                    softAssertions.assertThat(actualProfileImage).isNotEqualTo(expectedProfileImage);
+                });
+            }
+
+            @Test
+            void 닉네임과_프로필_사진_모두_바뀌면_모두_바뀐다() {
+                // given
+                final var beforeNickname = "before";
+                final var afterNickname = "after";
+                final var beforeProfileImage = "http://www.before.com";
+                final var afterProfileImage = "http://www.after.com";
+
+                final var request = new MemberRequest(afterNickname, afterProfileImage);
+
+                final var member = new Member(beforeNickname, beforeProfileImage, "1");
+                final var memberId = 단일_멤버_저장(member);
+
+                final var expected = memberRepository.findById(memberId).get();
+                final var expectedNickname = expected.getNickname();
+                final var expectedProfileImage = expected.getProfileImage();
+
+                // when
+                memberService.modify(memberId, request);
+                final var actual = memberRepository.findById(memberId).get();
+                final var actualNickname = actual.getNickname();
+                final var actualProfileImage = actual.getProfileImage();
+
+                // then
+                assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(actualNickname).isNotEqualTo(expectedNickname);
+                    softAssertions.assertThat(actualProfileImage).isNotEqualTo(expectedProfileImage);
+                });
+            }
         }
 
-        @Test
-        void 닉네임만_바뀌고_프로필_사진은_그대로면_닉네임만_바뀐다() {
-            // given
-            final var beforeNickname = "before";
-            final var profileImage = "http://www.test.com";
-            final var afterNickname = "after";
-            final var request = new MemberRequest(afterNickname, profileImage);
+        @Nested
+        class 실패_테스트 {
 
-            final var member = new Member(beforeNickname, profileImage, "1");
-            final var memberId = 단일_멤버_저장(member);
+            @Test
+            void 존재하지않는_멤버를_수정하면_예외가_발생한다() {
+                // given
+                final var beforeNickname = "before";
+                final var afterNickname = "after";
+                final var beforeProfileImage = "www.before.com";
+                final var afterProfileImage = "www.after.com";
 
-            final var expected = memberRepository.findById(memberId).get();
-            final var expectedNickname = expected.getNickname();
-            final var expectedProfileImage = expected.getProfileImage();
+                final var member = new Member(beforeNickname, beforeProfileImage, "1");
+                final var wrongMemberId = 단일_멤버_저장(member) + 1L;
 
-            // when
-            memberService.modify(memberId, request);
-            final var actual = memberRepository.findById(memberId).get();
-            final var actualNickname = actual.getNickname();
-            final var actualProfileImage = actual.getProfileImage();
+                final var request = new MemberRequest(afterNickname, afterProfileImage);
 
-            // then
-            assertSoftly(softAssertions -> {
-                softAssertions.assertThat(actualNickname).isNotEqualTo(expectedNickname);
-                softAssertions.assertThat(actualProfileImage).isEqualTo(expectedProfileImage);
-            });
-        }
+                // when
+                assertThatThrownBy(() -> memberService.modify(wrongMemberId, request))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
 
-        @Test
-        void 닉네임은_그대로이고_프로필_사진이_바뀌면_프로필_사진만_바뀐다() {
-            // given
-            final var nickname = "test";
-            final var beforeProfileImage = "http://www.before.com";
-            final var afterProfileImage = "http://www.after.com";
+            @Test
+            void 닉네임_수정_요청_값을_null로_설정하면_예외가_발생해야_하지만_통과하고_있다() {
+                // given
+                final var nickname = "test";
+                final var beforeProfileImage = "www.before.com";
+                final var afterProfileImage = "www.after.com";
 
-            final var request = new MemberRequest(nickname, afterProfileImage);
+                final var member = new Member(nickname, beforeProfileImage, "1");
+                final var memberId = 단일_멤버_저장(member);
 
-            final var member = new Member(nickname, beforeProfileImage, "1");
-            final var memberId = 단일_멤버_저장(member);
+                final var request = new MemberRequest(null, afterProfileImage);
 
-            final var expected = memberRepository.findById(memberId).get();
-            final var expectedNickname = expected.getNickname();
-            final var expectedProfileImage = expected.getProfileImage();
+                // when, then
+            }
 
-            // when
-            memberService.modify(memberId, request);
-            final var actual = memberRepository.findById(memberId).get();
-            final var actualNickname = actual.getNickname();
-            final var actualProfileImage = actual.getProfileImage();
+            @Test
+            void 프로필_이미지_요청_값을_null로_설정하면_예외가_발생해야_하지만_통과하고_있다() {
+                // given
+                final var beforeNickname = "before";
+                final var afterNickname = "after";
+                final var profileImage = "www.test.com";
 
-            // then
-            assertSoftly(softAssertions -> {
-                softAssertions.assertThat(actualNickname).isEqualTo(expectedNickname);
-                softAssertions.assertThat(actualProfileImage).isNotEqualTo(expectedProfileImage);
-            });
-        }
+                final var member = new Member(beforeNickname, profileImage, "1");
+                final var memberId = 단일_멤버_저장(member);
 
-        @Test
-        void 닉네임과_프로필_사진_모두_바뀌면_모두_바뀐다() {
-            // given
-            final var beforeNickname = "before";
-            final var afterNickname = "after";
-            final var beforeProfileImage = "http://www.before.com";
-            final var afterProfileImage = "http://www.after.com";
+                final var request = new MemberRequest(afterNickname, null);
 
-            final var request = new MemberRequest(afterNickname, afterProfileImage);
-
-            final var member = new Member(beforeNickname, beforeProfileImage, "1");
-            final var memberId = 단일_멤버_저장(member);
-
-            final var expected = memberRepository.findById(memberId).get();
-            final var expectedNickname = expected.getNickname();
-            final var expectedProfileImage = expected.getProfileImage();
-
-            // when
-            memberService.modify(memberId, request);
-            final var actual = memberRepository.findById(memberId).get();
-            final var actualNickname = actual.getNickname();
-            final var actualProfileImage = actual.getProfileImage();
-
-            // then
-            assertSoftly(softAssertions -> {
-                softAssertions.assertThat(actualNickname).isNotEqualTo(expectedNickname);
-                softAssertions.assertThat(actualProfileImage).isNotEqualTo(expectedProfileImage);
-            });
+                // when, then
+            }
         }
     }
 
