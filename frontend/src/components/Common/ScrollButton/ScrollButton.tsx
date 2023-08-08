bc541da4fb814ccbbde9a665b1824c694f889f2a
hae-on <solbi2004@naver.com>
2023-08-08 11:08:10 +0900

    feat: scroll button 구현

---
+++ frontend/src/components/Common/ScrollButton/ScrollButton.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@fun-eat/design-system';
+import { styled } from 'styled-components';
+
+import SvgIcon from '../Svg/SvgIcon';
+
+import useScroll from '@/hooks/useScroll';
+
+const ScrollButton = () => {
+  const { scrollToTop } = useScroll();
+
+  return (
+    <ScrollButtonWrapper customWidth="45px" customHeight="45px" variant="filled" color="gray5" onClick={scrollToTop}>
+      <SvgIcon variant="triangle" color="white" width={16} height={14} />
+    </ScrollButtonWrapper>
+  );
+};
+
+export default ScrollButton;
+
+const ScrollButtonWrapper = styled(Button)`
+  position: sticky;
+  bottom: 16px;
+  left: 95%;
+  border-radius: 50%;
+  z-index: 1;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: all 200ms ease-in-out;
+  }
+`;
