1346fc39d097d959c0d6ee7ec963961f2be1adca
TaeeunKim <xodms0309@naver.com>
2023-08-08 21:12:54 +0900

    chore: 리액트쿼리 초기 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,6 +8,9 @@ import MemberProvider from './contexts/MemberContext';
 import router from './router';
 import GlobalStyle from './styles';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 const main = async () => {
   if (process.env.NODE_ENV === 'development') {
     const { worker } = await import('./mocks/browser');
@@ -16,15 +19,20 @@ const main = async () => {
 };
 await main();
 
+export const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <MemberProvider>
-      <FunEatProvider>
-        <SvgSprite />
-        <GlobalStyle />
-        <RouterProvider router={router} />
-      </FunEatProvider>
-    </MemberProvider>
+    <QueryClientProvider client={queryClient}>
+      <MemberProvider>
+        <FunEatProvider>
+          <SvgSprite />
+          <GlobalStyle />
+          <RouterProvider router={router} />
+        </FunEatProvider>
+      </MemberProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
