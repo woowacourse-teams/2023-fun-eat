cdce989c1b23303a6248215a4383235d6f79a707
hanueleee <ezzeezz@naver.com>
2023-08-08 13:51:42 +0900

    test: 레시피 작성 기능에 대한 인수테스트 추가

---
+++ backend/src/test/java/com/funeat/acceptance/recipe/RecipeSteps.java
@@ -0,0 +1,40 @@
+package com.funeat.acceptance.recipe;
+
+import static io.restassured.RestAssured.given;
+
+import com.funeat.recipe.dto.RecipeCreateRequest;
+import io.restassured.builder.MultiPartSpecBuilder;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.MultiPartSpecification;
+import io.restassured.specification.RequestSpecification;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RecipeSteps {
+
+    public static ExtractableResponse<Response> 레시피_추가_요청(final RecipeCreateRequest recipeRequest,
+                                                          List<MultiPartSpecification> imageList,
+                                                          final String loginCookie) {
+        RequestSpecification request = given()
+                .cookie("JSESSIONID", loginCookie);
+        imageList.forEach(request::multiPart);
+        return request
+                .multiPart("recipeRequest", recipeRequest, "application/json")
+                .when()
+                .post("/api/recipes")
+                .then()
+                .extract();
+    }
+
+    public static List<MultiPartSpecification> 여러_사진_요청(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(i -> new MultiPartSpecBuilder("image".getBytes())
+                        .fileName("testImage.png")
+                        .controlName("images")
+                        .mimeType("image/png")
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
