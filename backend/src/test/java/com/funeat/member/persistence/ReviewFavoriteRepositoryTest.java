939d7130a5129f52a09e005c86ee25aa2c3c51cb
70825 <hello70825@gmail.com>
2023-08-08 14:02:01 +0900

    test: ReviewFavoriteRepositoryTest 실패 테스트 추가

--- backend/src/test/java/com/funeat/member/persistence/ReviewFavoriteRepositoryTest.java
+++ backend/src/test/java/com/funeat/member/persistence/ReviewFavoriteRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.funeat.member.persistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.funeat.common.RepositoryTest;
 import com.funeat.member.domain.Member;
@@ -9,36 +10,100 @@ import com.funeat.product.domain.Product;
 import com.funeat.review.domain.Review;
 import io.restassured.builder.MultiPartSpecBuilder;
 import io.restassured.specification.MultiPartSpecification;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NonAsciiCharacters")
 class ReviewFavoriteRepositoryTest extends RepositoryTest {
 
-    @Test
-    void 멤버와_리뷰로_리뷰_좋아요를_조회할_수_있다() {
-        // given
-        final var member = new Member("test", "image.png", "1");
-        단일_멤버_저장(member);
+    @Nested
+    class findByMemberAndReview_테스트 {
 
-        final var product = new Product("testName", 1000L, "test.png", "test", null);
-        단일_상품_저장(product);
+        @Nested
+        class 성공_테스트 {
 
-        final var image = 리뷰_사진_명세_요청();
-        final var review = new Review(member, product, image.getFileName(), 4L, "content", true);
-        단일_리뷰_저장(review);
+            @Test
+            void 멤버와_리뷰로_리뷰_좋아요를_조회할_수_있다() {
+                // given
+                final var member = new Member("test", "image.png", "1");
+                단일_멤버_저장(member);
 
-        final var reviewFavorite = ReviewFavorite.createReviewFavoriteByMemberAndReview(member, review, true);
-        단일_리뷰_좋아요_저장(reviewFavorite);
+                final var product = new Product("testName", 1000L, "test.png", "test", null);
+                단일_상품_저장(product);
 
-        final var expected = ReviewFavorite.createReviewFavoriteByMemberAndReview(member, review, true);
+                final var image = 리뷰_사진_명세_요청();
+                final var review = new Review(member, product, image.getFileName(), 4L, "content", true);
+                단일_리뷰_저장(review);
 
-        // when
-        final var actual = reviewFavoriteRepository.findByMemberAndReview(member, review).get();
+                final var reviewFavorite = ReviewFavorite.createReviewFavoriteByMemberAndReview(member, review, true);
+                단일_리뷰_좋아요_저장(reviewFavorite);
 
-        // then
-        assertThat(actual).usingRecursiveComparison()
-                .ignoringExpectedNullFields()
-                .isEqualTo(expected);
+                final var expected = ReviewFavorite.createReviewFavoriteByMemberAndReview(member, review, true);
+
+                // when
+                final var actual = reviewFavoriteRepository.findByMemberAndReview(member, review).get();
+
+                // then
+                assertThat(actual).usingRecursiveComparison()
+                        .ignoringExpectedNullFields()
+                        .isEqualTo(expected);
+            }
+        }
+
+        @Nested
+        class 실패_테스트 {
+
+            @Test
+            void 잘못된_멤버로_좋아요를_조회하면_에러가_발생한다() {
+                // given
+                final var member = new Member("test", "image.png", "1");
+                단일_멤버_저장(member);
+
+                final var product = new Product("testName", 1000L, "test.png", "test", null);
+                단일_상품_저장(product);
+
+                final var image = 리뷰_사진_명세_요청();
+                final var review = new Review(member, product, image.getFileName(), 4L, "content", true);
+                단일_리뷰_저장(review);
+
+                final var reviewFavorite = ReviewFavorite.createReviewFavoriteByMemberAndReview(member, review, true);
+                단일_리뷰_좋아요_저장(reviewFavorite);
+
+                final var wrongMember = new Member("wrong", "wrong.png", "2");
+                단일_멤버_저장(wrongMember);
+
+                // when, then
+                assertThatThrownBy(() -> reviewFavoriteRepository.findByMemberAndReview(wrongMember, review).get())
+                        .isInstanceOf(NoSuchElementException.class);
+            }
+
+            @Test
+            void 잘못된_리뷰로_좋아요를_조회하면_에러가_발생한다() {
+                // given
+                final var member = new Member("test", "image.png", "1");
+                단일_멤버_저장(member);
+                final var anotherMember = new Member("another", "another.png", "2");
+                단일_멤버_저장(anotherMember);
+
+                final var product = new Product("testName", 1000L, "test.png", "test", null);
+                단일_상품_저장(product);
+
+                final var image = 리뷰_사진_명세_요청();
+                final var review = new Review(member, product, image.getFileName(), 4L, "content", true);
+                단일_리뷰_저장(review);
+
+                final var reviewFavorite = ReviewFavorite.createReviewFavoriteByMemberAndReview(member, review, true);
+                단일_리뷰_좋아요_저장(reviewFavorite);
+
+                final var wrongReview = new Review(anotherMember, product, image.getFileName(), 5L, "content", false);
+                단일_리뷰_저장(wrongReview);
+
+                // when, then
+                assertThatThrownBy(() -> reviewFavoriteRepository.findByMemberAndReview(member, wrongReview).get())
+                        .isInstanceOf(NoSuchElementException.class);
+            }
+        }
     }
 
     private Long 단일_멤버_저장(final Member member) {
