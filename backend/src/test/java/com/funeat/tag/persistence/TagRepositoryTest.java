ed37a40c0a93212873283defa7a2e898aabbd82a
70825 <hello70825@gmail.com>
2023-08-09 14:58:24 +0900

    test: TagRepositoryTest 리팩터링

--- backend/src/test/java/com/funeat/tag/persistence/TagRepositoryTest.java
+++ backend/src/test/java/com/funeat/tag/persistence/TagRepositoryTest.java
@@ -1,7 +1,7 @@
 package com.funeat.tag.persistence;
 
-import static com.funeat.tag.domain.TagType.ETC;
-import static com.funeat.tag.domain.TagType.PRICE;
+import static com.funeat.fixture.TagFixture.태그_갓성비_PRICE_생성;
+import static com.funeat.fixture.TagFixture.태그_맛있어요_TASTE_생성;
 import static com.funeat.tag.domain.TagType.TASTE;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,46 +18,68 @@ class TagRepositoryTest extends RepositoryTest {
     @Nested
     class findTagsByIdIn_테스트 {
 
-        @Test
-        void 여러_태그_아이디로_태그들을_조회_할_수_있다() {
-            // given
-            final var tag1 = 단일_태그_저장(new Tag("testTag1", ETC));
-            final var tag2 = 단일_태그_저장(new Tag("testTag2", ETC));
-            final var tags = List.of(tag1, tag2);
-            final var tagIds = tags.stream()
-                    .map(Tag::getId)
-                    .collect(Collectors.toList());
+        @Nested
+        class 성공_테스트 {
 
-            // then
-            final var result = tagRepository.findTagsByIdIn(tagIds);
+            @Test
+            void 여러_태그_아이디로_태그들을_조회_할_수_있다() {
+                // given
+                final var tag1 = 태그_맛있어요_TASTE_생성();
+                final var tag2 = 태그_갓성비_PRICE_생성();
+                final var tags = List.of(tag1, tag2);
+                복수_태그_저장(tags);
 
-            // when
-            assertThat(result).usingRecursiveComparison()
-                    .isEqualTo(tags);
+                final var tagIds = 태그_아이디_변환(tags);
+
+                // then
+                final var result = tagRepository.findTagsByIdIn(tagIds);
+
+                // when
+                assertThat(result).usingRecursiveComparison().isEqualTo(tags);
+            }
+        }
+
+        @Nested
+        class 실패_테스트 {
         }
     }
 
     @Nested
     class findTagsByTagType_테스트 {
 
-        @Test
-        void 태그_타입으로_태그들을_조회할_수_있다() {
-            // given
-            final var tag1 = 단일_태그_저장(new Tag("단짠단짠", TASTE));
-            final var tag2 = 단일_태그_저장(new Tag("매콤해요", TASTE));
-            final var tag3 = 단일_태그_저장(new Tag("갓성비", PRICE));
-            final var expected = List.of(tag1, tag2);
+        @Nested
+        class 성공_테스트 {
 
-            // when
-            final var actual = tagRepository.findTagsByTagType(TASTE);
+            @Test
+            void 태그_타입으로_태그들을_조회할_수_있다() {
+                // given
+                final var tag1 = 태그_맛있어요_TASTE_생성();
+                final var tag2 = 태그_갓성비_PRICE_생성();
+                final var tags = List.of(tag1, tag2);
+                복수_태그_저장(tags);
 
-            // then
-            assertThat(actual).usingRecursiveComparison()
-                    .isEqualTo(expected);
+                final var expected = List.of(tag1);
+
+                // when
+                final var actual = tagRepository.findTagsByTagType(TASTE);
+
+                // then
+                assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+            }
+        }
+
+        @Nested
+        class 실패_테스트 {
         }
     }
 
-    private Tag 단일_태그_저장(final Tag tag) {
-        return tagRepository.save(tag);
+    private void 복수_태그_저장(final List<Tag> tags) {
+        tagRepository.saveAll(tags);
+    }
+
+    private List<Long> 태그_아이디_변환(final List<Tag> tags) {
+        return tags.stream()
+                .map(Tag::getId)
+                .collect(Collectors.toList());
     }
 }
