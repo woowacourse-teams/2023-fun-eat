f8b99ba0aa1e0bd1511e6d296b2405751b5fb405
Leejin-Yang <ejin1996@gmail.com>
2023-08-10 01:58:57 +0900

    test: 별점 커스텀 훅 테스트 수정

--- frontend/__tests__/hooks/useStarRating.test.ts
+++ frontend/__tests__/hooks/useStarRating.test.ts
@@ -1,20 +1,8 @@
-import useStarRating from '@/hooks/useStarRate';
+import { useStarRatingHover } from '@/hooks/review';
 import { renderHook, act } from '@testing-library/react';
 
-it('handleRating을 사용하여 별점을 저장할 수 있다.', () => {
-  const { result } = renderHook(() => useStarRating());
-
-  expect(result.current.rating).toBe(0);
-
-  act(() => {
-    result.current.handleRating(3);
-  });
-
-  expect(result.current.rating).toBe(3);
-});
-
 it('handleMouseEnter를 사용하여 마우스 호버된 별점 값을 저장할 수 있다.', () => {
-  const { result } = renderHook(() => useStarRating());
+  const { result } = renderHook(() => useStarRatingHover());
 
   expect(result.current.hovering).toBe(0);
 
@@ -26,7 +14,7 @@ it('handleMouseEnter를 사용하여 마우스 호버된 별점 값을 저장할
 });
 
 it('handleMouseLeave를 사용하여 마우스 호버된 별점을 초기화 할 수 있다.', () => {
-  const { result } = renderHook(() => useStarRating());
+  const { result } = renderHook(() => useStarRatingHover());
 
   expect(result.current.hovering).toBe(0);
 
