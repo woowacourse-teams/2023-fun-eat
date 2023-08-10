ec02b6c35b37be83af617a6cd2f7c74e30fa09c8
wugawuga <dnr3655@naver.com>
2023-08-11 01:44:04 +0900

    refactor: RecipeService 커스텀 예외로 수정

--- backend/src/main/java/com/funeat/recipe/application/RecipeService.java
+++ backend/src/main/java/com/funeat/recipe/application/RecipeService.java
@@ -1,8 +1,14 @@
 package com.funeat.recipe.application;
 
+import static com.funeat.member.exception.MemberErrorCode.MEMBER_NOF_FOUND;
+import static com.funeat.product.exception.ProductErrorCode.PRODUCT_NOF_FOUND;
+
+import com.funeat.common.ImageService;
 import com.funeat.member.domain.Member;
+import com.funeat.member.exception.MemberException.MemberNotFoundException;
 import com.funeat.member.persistence.MemberRepository;
 import com.funeat.product.domain.ProductRecipe;
+import com.funeat.product.exception.ProductException.ProductNotFoundException;
 import com.funeat.product.persistence.ProductRecipeRepository;
 import com.funeat.product.persistence.ProductRepository;
 import com.funeat.recipe.domain.Recipe;
@@ -10,7 +16,6 @@ import com.funeat.recipe.domain.RecipeImage;
 import com.funeat.recipe.dto.RecipeCreateRequest;
 import com.funeat.recipe.persistence.RecipeImageRepository;
 import com.funeat.recipe.persistence.RecipeRepository;
-import com.funeat.common.ImageService;
 import java.util.List;
 import java.util.Objects;
 import org.springframework.stereotype.Service;
@@ -44,13 +49,13 @@ public class RecipeService {
     @Transactional
     public Long create(final Long memberId, final List<MultipartFile> images, final RecipeCreateRequest request) {
         final Member member = memberRepository.findById(memberId)
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(() -> new MemberNotFoundException(MEMBER_NOF_FOUND, memberId));
 
         final Recipe savedRecipe = recipeRepository.save(new Recipe(request.getName(), request.getContent(), member));
         request.getProductIds()
                 .stream()
                 .map(it -> productRepository.findById(it)
-                        .orElseThrow(IllegalArgumentException::new))
+                        .orElseThrow(() -> new ProductNotFoundException(PRODUCT_NOF_FOUND, it)))
                 .forEach(it -> productRecipeRepository.save(new ProductRecipe(it, savedRecipe)));
 
         if (Objects.nonNull(images)) {
