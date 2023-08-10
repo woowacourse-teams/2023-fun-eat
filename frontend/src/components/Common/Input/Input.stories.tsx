5cb2a67146ea12079f56961dc3ec5568d8f82828
TaeeunKim <xodms0309@naver.com>
2023-08-10 13:41:45 +0900

    style: import 위치 수정

--- frontend/src/components/Common/Input/Input.stories.tsx
+++ frontend/src/components/Common/Input/Input.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import SvgIcon from '../Svg/SvgIcon';
 
 import Input from './Input';
+import SvgIcon from '../Svg/SvgIcon';
 
 const meta: Meta<typeof Input> = {
   title: 'common/Input',
