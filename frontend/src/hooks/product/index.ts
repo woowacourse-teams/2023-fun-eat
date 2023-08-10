06f65e053b5554025a6d4853fc3e4bcecfd88dfb
hae-on <solbi2004@naver.com>
2023-08-10 18:13:03 +0900

    chore: 사용하지 않는 파일 삭제

--- frontend/src/hooks/product/index.ts
+++ frontend/src/hooks/product/index.ts
@@ -1,4 +1,2 @@
-export { default as useProductReview } from './useProductReview';
-export { default as useProductDetail } from './useProductDetail';
 export { default as useInfiniteProductsQuery } from './useInfiniteProductsQuery';
 export { default as useInfiniteProductReviewsQuery } from './useInfiniteProductReviewsQuery';
