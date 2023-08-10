0e1c9555dd6ce9bbfae00bf8b6ba1333fcbf780c
Leejin-Yang <ejin1996@gmail.com>
2023-08-11 01:44:41 +0900

    feat: 검색 응답 타입 추가 및 PB 목 데이터 아이디 수정

--- frontend/src/hooks/queries/search/useSearchedProductsQuery.ts
+++ frontend/src/hooks/queries/search/useSearchedProductsQuery.ts
@@ -1,11 +1,11 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 import { searchApi } from '@/apis';
-import type { CategoryProductResponse } from '@/types/response';
+import type { SearchedProductResponse } from '@/types/response';
 
 const fetchSearchedProducts = async (query: string, page: number) => {
   const response = await searchApi.get({ params: '/products', queries: `?query=${query}&page=${page}` });
-  const data: CategoryProductResponse = await response.json();
+  const data: SearchedProductResponse = await response.json();
 
   return data;
 };
@@ -14,7 +14,7 @@ const useSearchedProductsQuery = (query: string) => {
   return useInfiniteQuery({
     queryKey: ['search', 'products', query],
     queryFn: ({ pageParam = 0 }) => fetchSearchedProducts(query, pageParam),
-    getNextPageParam: (prevResponse: CategoryProductResponse) => {
+    getNextPageParam: (prevResponse: SearchedProductResponse) => {
       const isLast = prevResponse.page.lastPage;
       const nextPage = prevResponse.page.requestPage + 1;
       return isLast ? undefined : nextPage;
--- frontend/src/mocks/handlers/searchHandlers.ts
+++ frontend/src/mocks/handlers/searchHandlers.ts
@@ -1,6 +1,6 @@
 import { rest } from 'msw';
 
-import commonProducts from '../data/products.json';
+import searchedProducts from '../data/searchedProducts.json';
 
 export const searchHandlers = [
   rest.get('/api/search/:searchId', (req, res, ctx) => {
@@ -14,8 +14,8 @@ export const searchHandlers = [
 
     if (searchId === 'products') {
       const filteredProducts = {
-        page: { ...commonProducts.page },
-        products: commonProducts.products
+        page: { ...searchedProducts.page },
+        products: searchedProducts.products
           .filter((product) => product.name.includes(query))
           .slice(page * 5, (page + 1) * 5),
       };
@@ -25,8 +25,8 @@ export const searchHandlers = [
     // TODO: 꿀조합 목 데이터 만들기
     if (searchId === 'recipes') {
       const filteredProducts = {
-        page: { ...commonProducts.page },
-        products: commonProducts.products.filter((product) => product.name.includes(query)),
+        page: { ...searchedProducts.page },
+        products: searchedProducts.products.filter((product) => product.name.includes(query)),
       };
       return res(ctx.status(200), ctx.json(filteredProducts));
     }
