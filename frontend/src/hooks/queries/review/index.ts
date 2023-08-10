f84109ceaf9eb8cf1255c6fe093189778c6b9d99
hae-on <solbi2004@naver.com>
2023-08-10 19:48:53 +0900

    refactor: review tag 리액트쿼리로 마이그레이션

---
+++ frontend/src/hooks/queries/review/index.ts
@@ -0,0 +1,1 @@
+export { default as useReviewTagsQuery } from './useReviewTagsQuery';
--- frontend/src/hooks/review/index.ts
+++ frontend/src/hooks/review/index.ts
@@ -3,5 +3,4 @@ export { default as useFormData } from './useFormData';
 export { default as useReviewFavorite } from './useReviewFavorite';
 export { default as useReviewImageUploader } from './useReviewImageUploader';
 export { default as useReviewRegisterFormMutation } from './useReviewRegisterFormMutation';
-export { default as useReviewTags } from './useReviewTags';
 export { default as useStarRatingHover } from './useStarRatingHover';
