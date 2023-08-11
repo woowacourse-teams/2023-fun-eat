52306fb0ac0bbd361afe637a09c829527e6caee5
hae-on <solbi2004@naver.com>
2023-08-11 13:22:30 +0900

    chore: 안쓰는 import 삭제

--- frontend/src/components/Rank/ProductRankingList/ProductRankingList.stories.tsx
+++ frontend/src/components/Rank/ProductRankingList/ProductRankingList.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import ProductRankingList from './ProductRankingList';
-import mockProductRankingList from '../../../mocks/data/productRankingList.json';
 
 const meta: Meta<typeof ProductRankingList> = {
   title: 'product/ProductRankingList',
