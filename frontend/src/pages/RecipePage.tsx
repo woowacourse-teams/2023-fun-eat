dc61a7d2296a96e6668ab6eb95af4b4c123f548f
Leejin-Yang <ejin1996@gmail.com>
2023-08-11 19:17:32 +0900

    feat: 꿀조합 작성하기 버튼 추가

--- frontend/src/pages/RecipePage.tsx
+++ frontend/src/pages/RecipePage.tsx
@@ -1,4 +1,5 @@
-import { BottomSheet, Heading, Link, useBottomSheet } from '@fun-eat/design-system';
+import { BottomSheet, Button, Heading, Link, Spacing, useBottomSheet } from '@fun-eat/design-system';
+import { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -6,10 +7,24 @@ import { ScrollButton, SortButton, SortOptionList, SvgIcon } from '@/components/
 import { RecipeList } from '@/components/Recipe';
 import { RECIPE_SORT_OPTIONS } from '@/constants';
 import { useSortOption } from '@/hooks/common';
+import { useMemberValueContext } from '@/hooks/context';
 
 const RecipePage = () => {
+  const [activeSheet, setActiveSheet] = useState<'registerRecipe' | 'sortOption'>('sortOption');
   const { selectedOption, selectSortOption } = useSortOption(RECIPE_SORT_OPTIONS[0]);
   const { ref, isClosing, handleOpenBottomSheet, handleCloseBottomSheet } = useBottomSheet();
+
+  const member = useMemberValueContext();
+
+  const handleOpenRegisterRecipeSheet = () => {
+    setActiveSheet('registerRecipe');
+    handleOpenBottomSheet();
+  };
+
+  const handleOpenSortOptionSheet = () => {
+    setActiveSheet('sortOption');
+    handleOpenBottomSheet();
+  };
 
   return (
     <>
@@ -22,17 +37,35 @@ const RecipePage = () => {
         </Link>
       </LinkWrapper>
       <SortButtonWrapper>
-        <SortButton option={selectedOption} onClick={handleOpenBottomSheet} />
+        <SortButton option={selectedOption} onClick={handleOpenSortOptionSheet} />
       </SortButtonWrapper>
       <RecipeList />
+      <Spacing size={80} />
+      <RecipeRegisterButtonWrapper>
+        <RecipeRegisterButton
+          type="button"
+          customWidth="100%"
+          customHeight="60px"
+          color={!member ? 'gray3' : 'primary'}
+          textColor={!member ? 'white' : 'default'}
+          size="lg"
+          weight="bold"
+          onClick={handleOpenRegisterRecipeSheet}
+        >
+          {member ? '꿀조합 작성하기' : '로그인 후 꿀조합을 작성할 수 있어요'}
+        </RecipeRegisterButton>
+      </RecipeRegisterButtonWrapper>
       <ScrollButton />
       <BottomSheet ref={ref} isClosing={isClosing} maxWidth="600px" close={handleCloseBottomSheet}>
-        <SortOptionList
-          options={RECIPE_SORT_OPTIONS}
-          selectedOption={selectedOption}
-          selectSortOption={selectSortOption}
-          close={handleCloseBottomSheet}
-        />
+        {activeSheet === 'sortOption' ? (
+          <SortOptionList
+            options={RECIPE_SORT_OPTIONS}
+            selectedOption={selectedOption}
+            selectSortOption={selectSortOption}
+            close={handleCloseBottomSheet}
+          />
+        ) : null}
+        {/*폼 추가되면 여기에 들어갈 예정*/}
       </BottomSheet>
     </>
   );
@@ -54,4 +87,21 @@ const SortButtonWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
   margin: 20px 0;
+`;
+
+const RecipeRegisterButtonWrapper = styled.div`
+  position: fixed;
+  bottom: 80px;
+  left: 20px;
+  width: calc(100% - 100px);
+  max-width: 500px;
+  height: 60px;
+
+  @media screen and (min-width: 600px) {
+    left: calc(50% - 280px);
+  }
+`;
+
+const RecipeRegisterButton = styled(Button)`
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
 `;
