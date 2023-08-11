0639f8c4d719e8adb8f18da736c2fd2a15dc1305
Leejin-Yang <ejin1996@gmail.com>
2023-08-11 15:08:51 +0900

    feat: RecipeItem 컴포넌트 추가

---
+++ frontend/src/components/Recipe/RecipeItem/RecipeItem.tsx
@@ -0,0 +1,88 @@
+import { Text, useTheme } from '@fun-eat/design-system';
+import styled from 'styled-components';
+
+import { SvgIcon } from '@/components/Common';
+import type { Recipe } from '@/types/recipe';
+
+interface RecipeItemProps {
+  recipe: Recipe;
+}
+
+const RecipeItem = ({ recipe }: RecipeItemProps) => {
+  const { image, title, author, createdAt, favoriteCount } = recipe;
+  const theme = useTheme();
+
+  return (
+    <RecipeItemContainer>
+      <ImageWrapper>
+        <RecipeImage src={image} />
+        <ProfileImage src={author.profileImage} />
+      </ImageWrapper>
+      <RecipeInfoWrapper>
+        <Text color={theme.textColors.sub}>
+          {author.nickname} 님 | {createdAt}
+        </Text>
+        <Text size="xl" weight="bold">
+          {title}
+        </Text>
+        <Text color={theme.textColors.info}>#불닭볶음면 #옥수수콘 #치즈...</Text>
+        <FavoriteWrapper>
+          <SvgIcon variant="favoriteFilled" width={16} height={16} />
+          <Text as="span" weight="bold">
+            {favoriteCount}
+          </Text>
+        </FavoriteWrapper>
+      </RecipeInfoWrapper>
+    </RecipeItemContainer>
+  );
+};
+
+export default RecipeItem;
+
+const RecipeItemContainer = styled.section`
+  height: 300px;
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 160px;
+`;
+
+const RecipeImage = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  object-fit: cover;
+`;
+
+const ProfileImage = styled.img`
+  position: absolute;
+  bottom: -20px;
+  right: 16px;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.backgroundColors.default};
+  border: 2px solid ${({ theme }) => theme.colors.primary};
+`;
+
+const RecipeInfoWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 120px;
+  margin-top: 20px;
+`;
+
+const FavoriteWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 0;
+  bottom: 50%;
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  transform: translateY(-50%);
+`;
