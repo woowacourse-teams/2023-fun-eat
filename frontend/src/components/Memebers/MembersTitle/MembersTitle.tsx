6627f0e56fa2ab9b0118f0f0cee4336be1b512db
hae-on <solbi2004@naver.com>
2023-08-12 17:46:38 +0900

    refactor: props로 경로 목적지 받기

--- frontend/src/components/Memebers/MembersTitle/MembersTitle.tsx
+++ frontend/src/components/Memebers/MembersTitle/MembersTitle.tsx
@@ -1,19 +1,22 @@
 import { Heading, Link, theme } from '@fun-eat/design-system';
+import { Link as RouterLink } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 import { SvgIcon } from '@/components/Common';
+import { PATH } from '@/constants/path';
 
 interface MembersTitleProps {
   title: string;
+  routeDestination: string;
 }
 
-const MembersTitle = ({ title }: MembersTitleProps) => {
+const MembersTitle = ({ title, routeDestination }: MembersTitleProps) => {
   return (
     <MemberTitleContainer>
       <Heading as="h2" size="xl">
         {title}
       </Heading>
-      <Link>
+      <Link as={RouterLink} to={`${PATH.PROFILE}/${routeDestination}`}>
         <ArrowIcon variant="arrow" color={theme.colors.gray5} width={18} height={18} />
       </Link>
     </MemberTitleContainer>
