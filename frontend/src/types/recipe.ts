6f9746a5714e9c7d08dc80783ac4b9a4c4409678
TaeeunKim <xodms0309@naver.com>
2023-08-13 14:46:03 +0900

    refactor: Recipe 관련 타입 리팩토링

--- frontend/src/types/recipe.ts
+++ frontend/src/types/recipe.ts
@@ -1,33 +1,20 @@
 import type { Member } from './member';
 import type { Product } from './product';
 
-export interface RecipeDetail {
-  id: number;
+export interface RecipeDetail extends Recipe {
   images: string[];
-  title: string;
   content: string;
-  author: Member;
-  products: UsedProduct[];
   totalPrice: number;
-  favoriteCount: number;
   favorite: boolean;
-  createdAt: string;
 }
-
-type UsedProduct = Pick<Product, 'id' | 'name' | 'price'>;
-
-interface RecipeProduct {
-  id: number;
-  name: string;
-  price: number;
-}
-
 export interface Recipe {
   id: number;
   image: string;
   title: string;
   author: Member;
-  createdAt: string;
-  favoriteCount: number;
   products: RecipeProduct[];
+  favoriteCount: number;
+  createdAt: string;
 }
+
+type RecipeProduct = Pick<Product, 'id' | 'name' | 'price'>;
