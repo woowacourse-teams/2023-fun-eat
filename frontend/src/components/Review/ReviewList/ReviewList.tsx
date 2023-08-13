bf2eb575d3f18f12fa21adbed5dd37bcf23c2f60
Leejin-Yang <ejin1996@gmail.com>
2023-08-13 15:12:30 +0900

    feat: 상품 리뷰 없을 때 처리

--- frontend/src/components/Review/ReviewList/ReviewList.tsx
+++ frontend/src/components/Review/ReviewList/ReviewList.tsx
@@ -1,3 +1,4 @@
+import { Text } from '@fun-eat/design-system';
 import { useRef } from 'react';
 import styled from 'styled-components';
 
@@ -18,16 +19,24 @@ const ReviewList = ({ productId, selectedOption }: ReviewListProps) => {
   const { fetchNextPage, hasNextPage, data } = useInfiniteProductReviewsQuery(productId, selectedOption.value);
   useIntersectionObserver<HTMLDivElement>(fetchNextPage, scrollRef, hasNextPage);
 
+  if (!data) {
+    return null;
+  }
+
+  const reviews = data.pages.flatMap((page) => page.reviews);
+
+  if (reviews.length === 0) {
+    return <Text>상품의 첫 리뷰를 작성해주세요</Text>;
+  }
+
   return (
     <>
       <ReviewListContainer>
-        {data?.pages
-          .flatMap((page) => page.reviews)
-          .map((review) => (
-            <li key={review.id}>
-              <ReviewItem productId={productId} review={review} />
-            </li>
-          ))}
+        {reviews.map((review) => (
+          <li key={review.id}>
+            <ReviewItem productId={productId} review={review} />
+          </li>
+        ))}
       </ReviewListContainer>
       <div ref={scrollRef} aria-hidden />
     </>
