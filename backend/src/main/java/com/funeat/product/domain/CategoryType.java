ab3c93db917fbb4e0cb407134dfc11610b62b374
JFe <33208246+Go-Jaecheol@users.noreply.github.com>
2023-08-14 15:27:46 +0900

    [BE] refactor: 상품 랭킹 조회 시 Response에 카테고리 타입 추가 (#421)
    * refactor: RankingProductDto에 categoryType 추가
    
    * refactor: CategoryType enum에 name 필드 추가

--- backend/src/main/java/com/funeat/product/domain/CategoryType.java
+++ backend/src/main/java/com/funeat/product/domain/CategoryType.java
@@ -6,12 +6,25 @@ import com.funeat.product.exception.CategoryException.CategoryTypeNotFoundExcept
 import java.util.Arrays;
 
 public enum CategoryType {
-    FOOD, STORE;
+
+    FOOD("food"),
+    STORE("store"),
+    ;
+
+    private final String name;
+
+    CategoryType(final String name) {
+        this.name = name;
+    }
 
     public static CategoryType findCategoryType(final String type) {
         return Arrays.stream(values())
                 .filter(it -> it.name().equals(type.toUpperCase()))
                 .findFirst()
                 .orElseThrow(() -> new CategoryTypeNotFoundException(CATEGORY_TYPE_NOT_FOUND, type));
+    }
+
+    public String getName() {
+        return name;
     }
 }
