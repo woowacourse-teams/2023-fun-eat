0c5f6182e913fe8e54d5f06c8b7932666354ffc9
hae-on <solbi2004@naver.com>
2023-08-14 16:08:17 +0900

    feat: recipe 추가

--- frontend/src/pages/MemberPage.tsx
+++ frontend/src/pages/MemberPage.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { NavigableSectionTitle } from '@/components/Common';
-import { MembersInfo, MemberReviewList } from '@/components/Members';
+import { MembersInfo, MemberReviewList, MemberRecipeList } from '@/components/Members';
 import { PATH } from '@/constants/path';
 import { useMember } from '@/hooks/auth';
 import { useMemberValueContext } from '@/hooks/context';
@@ -27,6 +27,11 @@ const MemberPage = () => {
       <NavigableSectionTitle title="내가 작성한 리뷰" routeDestination={`${PATH.MEMBER}/review`} />
       <Spacing size={5} />
       <MemberReviewList isMemberPage />
+      <Spacing size={40} />
+      <NavigableSectionTitle title="내가 작성한 꿀조합" routeDestination={`${PATH.MEMBER}/recipe`} />
+      <Spacing size={5} />
+      <MemberRecipeList isMemberPage />
+      <Spacing size={40} />
     </>
   );
 };
