57f659dc1239e1cf5a84b426891c32e0aff4c3ff
hanueleee <ezzeezz@naver.com>
2023-08-15 17:56:07 +0900

    test: RecipeFavoriteRepository의 findByMemberAndRecipe 메소드에 대한 테스트 구현

--- backend/src/test/java/com/funeat/member/persistence/RecipeFavoriteRepositoryTest.java
+++ backend/src/test/java/com/funeat/member/persistence/RecipeFavoriteRepositoryTest.java
@@ -50,7 +50,33 @@ class RecipeFavoriteRepositoryTest {
     private RecipeFavoriteRepository recipeFavoriteRepository;
 
     @Test
-    void 사용자의_레시피에_대한_좋아요_현황을_알_수_있다() {
+    void 해당_사용자의_해당_레시피에_대한_좋아요_현황을_반환_수_있다() {
+        // given
+        final var category = 카테고리_추가_요청(new Category("간편식사", CategoryType.FOOD));
+        final var product1 = 상품_추가_요청(new Product("불닭볶음면", 1000L, "image.png", "엄청 매운 불닭", category));
+        final var product2 = 상품_추가_요청(new Product("참치 삼김", 2000L, "image.png", "담백한 참치마요 삼김", category));
+        final var product3 = 상품_추가_요청(new Product("스트링 치즈", 1500L, "image.png", "고소한 치즈", category));
+        final var recipeAuthor = 멤버_추가_요청(new Member("author", "image.png", "1"));
+        final var products = List.of(product1, product2, product3);
+
+        final var recipe = 레시피_추가_요청(new Recipe("레시피1", "밥 넣고 밥 넣자", recipeAuthor));
+        products.forEach(it -> 레시피에_사용된_상품_추가_요청(new ProductRecipe(it, recipe)));
+
+        // when
+        final var realMember = 멤버_추가_요청(new Member("real", "image.png", "2"));
+        final var fakeMember = 멤버_추가_요청(new Member("fake", "image.png", "3"));
+        레시피_좋아요_요청(new RecipeFavorite(realMember, recipe, true));
+
+        final var realMemberActual = recipeFavoriteRepository.findByMemberAndRecipe(realMember, recipe);
+        final var fakeMemberActual = recipeFavoriteRepository.findByMemberAndRecipe(fakeMember, recipe);
+
+        // then
+        assertThat(realMemberActual).isNotEmpty();
+        assertThat(fakeMemberActual).isEmpty();
+    }
+
+    @Test
+    void 해당_사용자가_해당_레시피에_좋아요를_눌렀는지_알_수_있다() {
         // given
         final var category = 카테고리_추가_요청(new Category("간편식사", CategoryType.FOOD));
         final var product1 = 상품_추가_요청(new Product("불닭볶음면", 1000L, "image.png", "엄청 매운 불닭", category));
