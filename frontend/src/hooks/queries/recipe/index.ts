7add336b6bcb0e51bfab8b610455f9d2a081cd86
Leejin Yang <ejin1996@gmail.com>
2023-08-15 13:59:35 +0900

    [FE] feat: 꿀조합 전체 목록 조회 요청 쿼리 추가 (#449)
    * feat: 꿀조합 정렬 옵션 타입 추가
    
    * feat: 꿀조합 전체 목록 조회 목 핸들러 추가
    
    * feat: 상품 리뷰 조회 최신순 정렬 로직 추가
    
    * feat: 꿀조합 전체 목록 조회 쿼리 추가
    
    * refactor: 꿀조합 목 핸들러 중복 삭제

--- frontend/src/hooks/queries/recipe/index.ts
+++ frontend/src/hooks/queries/recipe/index.ts
@@ -1,2 +1,3 @@
 export { default as useRecipeDetailQuery } from './useRecipeDetailQuery';
 export { default as useRecipeRegisterFormMutation } from './useRecipeRegisterFormMutation';
+export { default as useInfiniteRecipesQuery } from './useInfiniteRecipesQuery';
