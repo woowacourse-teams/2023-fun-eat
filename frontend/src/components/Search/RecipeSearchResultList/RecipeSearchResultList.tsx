073bac2b8030e77e71126966beb45cf190dc4b9d
Leejin-Yang <ejin1996@gmail.com>
2023-08-15 21:13:41 +0900

    feat: 꿀조합 검색 결과 리스트 컴포넌트 추가

---
+++ frontend/src/components/Search/RecipeSearchResultList/RecipeSearchResultList.tsx
@@ -0,0 +1,52 @@
+import { Link, Text } from '@fun-eat/design-system';
+import { useRef } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+import { RecipeItem } from '@/components/Recipe';
+import { PATH } from '@/constants/path';
+import { useIntersectionObserver } from '@/hooks/common';
+import { useInfiniteRecipeSearchResultsQuery } from '@/hooks/queries/search';
+
+interface RecipeSearchResultListProps {
+  searchQuery: string;
+}
+
+const RecipeSearchResultList = ({ searchQuery }: RecipeSearchResultListProps) => {
+  const { data: searchResponse, fetchNextPage, hasNextPage } = useInfiniteRecipeSearchResultsQuery(searchQuery);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  useIntersectionObserver<HTMLDivElement>(fetchNextPage, scrollRef, hasNextPage);
+
+  if (!searchResponse) {
+    return null;
+  }
+
+  const recipes = searchResponse.pages.flatMap((page) => page.recipes);
+
+  if (recipes.length === 0) {
+    return <Text>검색한 꿀조합을 찾을 수 없습니다.</Text>;
+  }
+
+  return (
+    <>
+      <RecipeSearchResultListContainer>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <Link as={RouterLink} to={`${PATH.RECIPE}/${recipe.id}`}>
+              <RecipeItem recipe={recipe} />
+            </Link>
+          </li>
+        ))}
+      </RecipeSearchResultListContainer>
+      <div ref={scrollRef} aria-hidden />
+    </>
+  );
+};
+
+export default RecipeSearchResultList;
+
+const RecipeSearchResultListContainer = styled.ul`
+  & > li + li {
+    margin-top: 40px;
+  }
+`;
--- frontend/src/components/Search/index.ts
+++ frontend/src/components/Search/index.ts
@@ -1,2 +1,3 @@
 export { default as SearchResultList } from './SearchResultList/SearchResultList';
 export { default as RecommendList } from './RecommendList/RecommendList';
+export { default as RecipeSearchResultList } from './RecipeSearchResultList/RecipeSearchResultList';
