dcffb149787703d328af07a1c6b2b520d23f92b9
Go-Jaecheol <gojaech@naver.com>
2023-08-16 18:49:32 +0900

    feat: 검색어가 포함된 상품이 있는 꿀조합 목록 조회하는 RecipeRepository 메서드 추가

--- backend/src/main/java/com/funeat/recipe/persistence/RecipeRepository.java
+++ backend/src/main/java/com/funeat/recipe/persistence/RecipeRepository.java
@@ -5,8 +5,18 @@ import com.funeat.recipe.domain.Recipe;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     Page<Recipe> findRecipesByMember(final Member member, final Pageable pageable);
+
+    @Query("SELECT r FROM Recipe r "
+            + "LEFT JOIN ProductRecipe pr ON pr.recipe.id = r.id "
+            + "WHERE pr.product.name LIKE CONCAT('%', :name, '%') "
+            + "ORDER BY CASE "
+            + "WHEN pr.product.name LIKE CONCAT(:name, '%') THEN 1 "
+            + "ELSE 2 END")
+    Page<Recipe> findAllByProductNameContaining(@Param("name") final String name, final Pageable pageable);
 }
