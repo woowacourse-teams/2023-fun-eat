db3b75c66f0953168005fd549bcf8501f684ea39
hanueleee <ezzeezz@naver.com>
2023-08-16 16:03:30 +0900

    chore: 컨벤션 적용

--- backend/src/test/java/com/funeat/member/persistence/RecipeFavoriteRepositoryTest.java
+++ backend/src/test/java/com/funeat/member/persistence/RecipeFavoriteRepositoryTest.java
@@ -17,6 +17,7 @@ import com.funeat.common.RepositoryTest;
 import com.funeat.member.domain.favorite.RecipeFavorite;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -29,85 +30,93 @@ import org.springframework.context.annotation.Import;
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class RecipeFavoriteRepositoryTest extends RepositoryTest {
 
-    @Test
-    void 해당_사용자의_해당_레시피에_대한_좋아요_현황을_반환할_수_있다() {
-        // given
-        final var category = 카테고리_즉석조리_생성();
-        단일_카테고리_저장(category);
+    @Nested
+    class findByMemberAndRecipe_성공_테스트 {
 
-        final var product1 = 상품_삼각김밥_가격1000원_평점1점_생성(category);
-        final var product2 = 상품_삼각김밥_가격1000원_평점2점_생성(category);
-        final var product3 = 상품_삼각김밥_가격3000원_평점2점_생성(category);
-        복수_상품_저장(product1, product2, product3);
+        @Test
+        void 해당_사용자의_해당_레시피에_대한_좋아요_현황을_반환할_수_있다() {
+            // given
+            final var category = 카테고리_즉석조리_생성();
+            단일_카테고리_저장(category);
 
-        final var recipeAuthor = 멤버_멤버1_생성();
-        단일_멤버_저장(recipeAuthor);
+            final var product1 = 상품_삼각김밥_가격1000원_평점1점_생성(category);
+            final var product2 = 상품_삼각김밥_가격1000원_평점2점_생성(category);
+            final var product3 = 상품_삼각김밥_가격3000원_평점2점_생성(category);
+            복수_상품_저장(product1, product2, product3);
 
-        final var recipe = 레시피_생성(recipeAuthor);
-        단일_레시피_저장(recipe);
+            final var recipeAuthor = 멤버_멤버1_생성();
+            단일_멤버_저장(recipeAuthor);
 
-        final var productRecipe1 = 레시피_안에_들어가는_상품_생성(product1, recipe);
-        final var productRecipe2 = 레시피_안에_들어가는_상품_생성(product2, recipe);
-        final var productRecipe3 = 레시피_안에_들어가는_상품_생성(product3, recipe);
-        복수_레시피_상품_저장(productRecipe1, productRecipe2, productRecipe3);
+            final var recipe = 레시피_생성(recipeAuthor);
+            단일_레시피_저장(recipe);
 
-        final var realMember = 멤버_멤버2_생성();
-        final var fakeMember = 멤버_멤버3_생성();
-        복수_멤버_저장(realMember, fakeMember);
-        레시피_좋아요_저장(new RecipeFavorite(realMember, recipe, true));
+            final var productRecipe1 = 레시피_안에_들어가는_상품_생성(product1, recipe);
+            final var productRecipe2 = 레시피_안에_들어가는_상품_생성(product2, recipe);
+            final var productRecipe3 = 레시피_안에_들어가는_상품_생성(product3, recipe);
+            복수_레시피_상품_저장(productRecipe1, productRecipe2, productRecipe3);
 
-        // when
-        final var realMemberActual = recipeFavoriteRepository.findByMemberAndRecipe(realMember, recipe);
-        final var fakeMemberActual = recipeFavoriteRepository.findByMemberAndRecipe(fakeMember, recipe);
+            final var realMember = 멤버_멤버2_생성();
+            final var fakeMember = 멤버_멤버3_생성();
+            복수_멤버_저장(realMember, fakeMember);
+            레시피_좋아요_저장(new RecipeFavorite(realMember, recipe, true));
 
-        // then
-        assertSoftly(softAssertions -> {
-            softAssertions.assertThat(realMemberActual)
-                    .isNotEmpty();
-            softAssertions.assertThat(fakeMemberActual)
-                    .isEmpty();
-        });
+            // when
+            final var realMemberActual = recipeFavoriteRepository.findByMemberAndRecipe(realMember, recipe);
+            final var fakeMemberActual = recipeFavoriteRepository.findByMemberAndRecipe(fakeMember, recipe);
+
+            // then
+            assertSoftly(softAssertions -> {
+                softAssertions.assertThat(realMemberActual)
+                        .isNotEmpty();
+                softAssertions.assertThat(fakeMemberActual)
+                        .isEmpty();
+            });
+        }
     }
 
-    @Test
-    void 해당_사용자가_해당_레시피에_좋아요를_눌렀는지_알_수_있다() {
-        // given
-        final var category = 카테고리_즉석조리_생성();
-        단일_카테고리_저장(category);
+    @Nested
+    class existsByMemberAndRecipeAndFavoriteTrue_성공_테스트 {
 
-        final var product1 = 상품_삼각김밥_가격1000원_평점1점_생성(category);
-        final var product2 = 상품_삼각김밥_가격1000원_평점2점_생성(category);
-        final var product3 = 상품_삼각김밥_가격3000원_평점2점_생성(category);
-        복수_상품_저장(product1, product2, product3);
+        @Test
+        void 해당_사용자가_해당_레시피에_좋아요를_눌렀는지_알_수_있다() {
+            // given
+            final var category = 카테고리_즉석조리_생성();
+            단일_카테고리_저장(category);
 
-        final var recipeAuthor = 멤버_멤버1_생성();
-        단일_멤버_저장(recipeAuthor);
+            final var product1 = 상품_삼각김밥_가격1000원_평점1점_생성(category);
+            final var product2 = 상품_삼각김밥_가격1000원_평점2점_생성(category);
+            final var product3 = 상품_삼각김밥_가격3000원_평점2점_생성(category);
+            복수_상품_저장(product1, product2, product3);
 
-        final var recipe = 레시피_생성(recipeAuthor);
-        단일_레시피_저장(recipe);
+            final var recipeAuthor = 멤버_멤버1_생성();
+            단일_멤버_저장(recipeAuthor);
 
-        final var productRecipe1 = 레시피_안에_들어가는_상품_생성(product1, recipe);
-        final var productRecipe2 = 레시피_안에_들어가는_상품_생성(product2, recipe);
-        final var productRecipe3 = 레시피_안에_들어가는_상품_생성(product3, recipe);
-        복수_레시피_상품_저장(productRecipe1, productRecipe2, productRecipe3);
+            final var recipe = 레시피_생성(recipeAuthor);
+            단일_레시피_저장(recipe);
 
-        final var realMember = 멤버_멤버2_생성();
-        final var fakeMember = 멤버_멤버3_생성();
-        복수_멤버_저장(realMember, fakeMember);
-        레시피_좋아요_저장(new RecipeFavorite(realMember, recipe, true));
+            final var productRecipe1 = 레시피_안에_들어가는_상품_생성(product1, recipe);
+            final var productRecipe2 = 레시피_안에_들어가는_상품_생성(product2, recipe);
+            final var productRecipe3 = 레시피_안에_들어가는_상품_생성(product3, recipe);
+            복수_레시피_상품_저장(productRecipe1, productRecipe2, productRecipe3);
 
-        // when
-        final var realMemberActual = recipeFavoriteRepository.existsByMemberAndRecipeAndFavoriteTrue(realMember,
-                recipe);
-        final var fakeMemberActual = recipeFavoriteRepository.existsByMemberAndRecipeAndFavoriteTrue(fakeMember,
-                recipe);
+            final var realMember = 멤버_멤버2_생성();
+            final var fakeMember = 멤버_멤버3_생성();
+            복수_멤버_저장(realMember, fakeMember);
+            레시피_좋아요_저장(new RecipeFavorite(realMember, recipe, true));
 
-        // then
-        assertSoftly(softAssertions -> {
-            softAssertions.assertThat(realMemberActual)
-                    .isTrue();
-            softAssertions.assertThat(fakeMemberActual)
-                    .isFalse();
-        });
+            // when
+            final var realMemberActual = recipeFavoriteRepository.existsByMemberAndRecipeAndFavoriteTrue(realMember,
+                    recipe);
+            final var fakeMemberActual = recipeFavoriteRepository.existsByMemberAndRecipeAndFavoriteTrue(fakeMember,
+                    recipe);
+
+            // then
+            assertSoftly(softAssertions -> {
+                softAssertions.assertThat(realMemberActual)
+                        .isTrue();
+                softAssertions.assertThat(fakeMemberActual)
+                        .isFalse();
+            });
+        }
     }
 }
