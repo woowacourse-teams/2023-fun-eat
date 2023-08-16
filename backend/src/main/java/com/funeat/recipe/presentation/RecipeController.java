5d132b22e09e0e988f359e4c806d43b86c8d1149
Go-Jaecheol <gojaech@naver.com>
2023-08-16 18:47:26 +0900

    feat: 꿀조합 검색 기능 controller 및 dto 추가

--- backend/src/main/java/com/funeat/recipe/presentation/RecipeController.java
+++ backend/src/main/java/com/funeat/recipe/presentation/RecipeController.java
@@ -5,16 +5,20 @@ import com.funeat.auth.util.AuthenticationPrincipal;
 import com.funeat.recipe.dto.RecipeCreateRequest;
 import com.funeat.recipe.dto.RecipeDetailResponse;
 import com.funeat.recipe.dto.RecipeFavoriteRequest;
+import com.funeat.recipe.dto.SearchRecipeResultsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -49,4 +53,13 @@ public interface RecipeController {
     ResponseEntity<Void> likeRecipe(@AuthenticationPrincipal final LoginInfo loginInfo,
                                     @PathVariable final Long recipeId,
                                     @RequestBody RecipeFavoriteRequest request);
+
+    @Operation(summary = "꿀조합 검색 결과 조회", description = "검색어가 포함된 상품이 있는 꿀조합 목록을 조회한다.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "꿀조합 검색 결과 조회 성공."
+    )
+    @GetMapping
+    ResponseEntity<SearchRecipeResultsResponse> getSearchResults(@RequestParam final String query,
+                                                                 @PageableDefault final Pageable pageable);
 }
