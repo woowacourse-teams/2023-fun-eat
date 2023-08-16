36830fccdddac7fdafaa13e22905bd6b3844dc7a
hanueleee <ezzeezz@naver.com>
2023-08-16 18:16:06 +0900

    feat: RecipeDetailHandlerInterceptor 추가

---
+++ backend/src/main/java/com/funeat/recipe/util/RecipeDetailHandlerInterceptor.java
@@ -0,0 +1,22 @@
+package com.funeat.recipe.util;
+
+import com.funeat.auth.exception.AuthErrorCode;
+import com.funeat.auth.exception.AuthException.NotLoggedInException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class RecipeDetailHandlerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        final HttpSession session = request.getSession();
+        if (session.getAttribute("member") == null) {
+            throw new NotLoggedInException(AuthErrorCode.LOGIN_MEMBER_NOT_FOUND);
+        }
+        return true;
+    }
+}
