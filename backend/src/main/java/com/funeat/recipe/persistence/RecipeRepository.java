652145abc8bdf85fc05852e28142aa5a5a12d419
Go-Jaecheol <gojaech@naver.com>
2023-08-17 04:19:00 +0900

    feat: 좋아요 내림차순으로 꿀조합 조회하는 repository 메서드 추가

--- backend/src/main/java/com/funeat/recipe/persistence/RecipeRepository.java
+++ backend/src/main/java/com/funeat/recipe/persistence/RecipeRepository.java
@@ -2,6 +2,7 @@ package com.funeat.recipe.persistence;
 
 import com.funeat.member.domain.Member;
 import com.funeat.recipe.domain.Recipe;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     Page<Recipe> findRecipesByMember(final Member member, final Pageable pageable);
 
     Page<Recipe> findAll(final Pageable pageable);
+
+    List<Recipe> findRecipesByOrderByFavoriteCountDesc(final Pageable pageable);
 }
