09070f46f03f83d3bded5f29c3e3da90028c2fe8
Go-Jaecheol <gojaech@naver.com>
2023-08-17 11:13:40 +0900

    feat: 상품으로 꿀조합 목록 조회하는 repository 메서드 추가

--- backend/src/main/java/com/funeat/recipe/persistence/RecipeRepository.java
+++ backend/src/main/java/com/funeat/recipe/persistence/RecipeRepository.java
@@ -1,14 +1,19 @@
 package com.funeat.recipe.persistence;
 
 import com.funeat.member.domain.Member;
+import com.funeat.product.domain.Product;
 import com.funeat.recipe.domain.Recipe;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     Page<Recipe> findRecipesByMember(final Member member, final Pageable pageable);
 
     Page<Recipe> findAll(final Pageable pageable);
+
+    @Query("SELECT r FROM Recipe r LEFT JOIN ProductRecipe pr ON pr.product = :product WHERE pr.recipe.id = r.id")
+    Page<Recipe> findRecipesByProduct(final Product product, final Pageable pageable);
 }
