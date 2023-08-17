ac4c23c03c8f01fc24d85befb0898a3a55589fb1
hae-on <solbi2004@naver.com>
2023-08-17 16:39:46 +0900

    refactor: 로그인 로직 수정

--- frontend/src/mocks/handlers/logoutHandlers.ts
+++ frontend/src/mocks/handlers/logoutHandlers.ts
@@ -2,6 +2,10 @@ import { rest } from 'msw';
 
 export const logoutHandlers = [
   rest.post('/api/logout', (req, res, ctx) => {
-    return res(ctx.status(302), ctx.set('Location', '/'));
+    return res(
+      ctx.status(302),
+      ctx.cookie('mockSessionId', '', { path: '/', expires: new Date(0) }),
+      ctx.set('Location', '/')
+    );
   }),
 ];
