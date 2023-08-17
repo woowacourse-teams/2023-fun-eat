fcaa2604b97879a6e509b9c8432a6790717514a4
TaeeunKim <xodms0309@naver.com>
2023-08-17 22:31:56 +0900

    refactor: IconWrapper을 button에서 div로 변경

--- frontend/src/components/Common/Input/Input.tsx
+++ frontend/src/components/Common/Input/Input.tsx
@@ -31,7 +31,7 @@ const Input = forwardRef(
       <>
         <InputContainer customWidth={customWidth}>
           <CustomInput ref={ref} isError={isError} {...props} />
-          {rightIcon && <IconWrapper variant="transparent">{rightIcon}</IconWrapper>}
+          {rightIcon && <IconWrapper>{rightIcon}</IconWrapper>}
         </InputContainer>
         {isError && <ErrorMessage>{errorMessage}</ErrorMessage>}
       </>
@@ -76,7 +76,9 @@ const CustomInput = styled.input<CustomInputStyleProps>`
   }
 `;
 
-const IconWrapper = styled(Button)`
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
   position: absolute;
   top: 0;
   right: 0;
