d01e786325b504c3f825bc230dc2f820b6cec03a
TaeeunKim <xodms0309@naver.com>
2023-08-17 16:17:31 +0900

    style: 개행 추가

--- frontend/src/mocks/handlers/rankingHandlers.ts
+++ frontend/src/mocks/handlers/rankingHandlers.ts
@@ -8,9 +8,11 @@ export const rankingHandlers = [
   rest.get('/api/ranks/products', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockProductRankingList), ctx.delay(1000));
   }),
+
   rest.get('/api/ranks/reviews', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockReviewRankingList), ctx.delay(1000));
   }),
+
   rest.get('/api/ranks/recipes', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockRecipeRankingList), ctx.delay(1000));
   }),
