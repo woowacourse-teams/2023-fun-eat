9bc17486fa36ee62a15fbecc5e83202be20771ea
TaeeunKim <xodms0309@naver.com>
2023-08-17 10:51:05 +0900

    feat: 꿀조합 랭킹 mocking 함수 추가

--- frontend/src/mocks/handlers/rankingHandlers.ts
+++ frontend/src/mocks/handlers/rankingHandlers.ts
@@ -1,6 +1,7 @@
 import { rest } from 'msw';
 
 import mockProductRankingList from '../data/productRankingList.json';
+import mockRecipeRankingList from '../data/recipeRankingList.json';
 import mockReviewRankingList from '../data/reviewRankingList.json';
 
 export const rankingHandlers = [
@@ -9,5 +10,8 @@ export const rankingHandlers = [
   }),
   rest.get('/api/ranks/reviews', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockReviewRankingList), ctx.delay(1000));
+  }),
+  rest.get('/api/rnaks/recipes', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockRecipeRankingList), ctx.delay(1000));
   }),
 ];
