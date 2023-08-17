51ec09782caa3233b7a1f0d69ab599fa87dddc32
Taeeun Kim <xodms0309@naver.com>
2023-08-17 20:22:29 +0900

    [FE] feat: 마이페이지 - 리뷰/꿀조합 없을 때 처리 (#519)
    * feat: 리뷰 없을 때 예외처리
    
    * feat: 꿀조합 없을 때 예외처리
    
    * style: 문구에 개행 추가
    
    * style: 따옴표 제거
    
    * fix: 캐러셀 너비 수정

--- frontend/src/components/Members/MemberReviewList/MemberReviewList.tsx
+++ frontend/src/components/Members/MemberReviewList/MemberReviewList.tsx
@@ -1,8 +1,10 @@
-import { Spacing, Text, theme } from '@fun-eat/design-system';
+import { Link, Spacing, Text, theme } from '@fun-eat/design-system';
 import { useRef } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { ReviewRankingItem } from '@/components/Rank';
+import { PATH } from '@/constants/path';
 import { useIntersectionObserver } from '@/hooks/common';
 import { useInfiniteMemberReviewQuery } from '@/hooks/queries/members';
 import useDisplaySlice from '@/utils/displaySlice';
@@ -21,15 +23,25 @@ const MemberReviewList = ({ isMemberPage = false }: MemberReviewListProps) => {
 
   const totalReviewCount = data.pages[0].page.totalDataCount;
 
-  if (memberReviews?.length === 0) {
-    return <Text>앗, 작성한 리뷰가 없어요</Text>;
+  if (totalReviewCount === 0) {
+    return (
+      <ErrorContainer>
+        <Text size="lg" weight="bold">
+          앗, 작성한 리뷰가 없네요 🥲
+        </Text>
+        <Spacing size={16} />
+        <ReviewLink as={RouterLink} to={`${PATH.PRODUCT_LIST}/food`} block>
+          리뷰 작성하러 가기
+        </ReviewLink>
+      </ErrorContainer>
+    );
   }
 
   return (
     <MemberReviewListContainer>
       {!isMemberPage && (
         <TotalReviewCount color={theme.colors.gray4}>
-          총 <strong>&apos;{totalReviewCount}&apos;</strong>개의 리뷰를 남겼어요!
+          총 <strong>{totalReviewCount}</strong>개의 리뷰를 남겼어요!
         </TotalReviewCount>
       )}
       <Spacing size={20} />
@@ -60,4 +72,17 @@ const MemberReviewListWrapper = styled.ul`
 
 const TotalReviewCount = styled(Text)`
   text-align: right;
+`;
+
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const ReviewLink = styled(Link)`
+  padding: 12px 12px;
+  border: 1px solid ${({ theme }) => theme.colors.gray4};
+  border-radius: 8px;
 `;
