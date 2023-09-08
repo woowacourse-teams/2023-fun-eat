4edfd144b1ba3890404244d8b37be16ea560686b
Leejin-Yang <ejin1996@gmail.com>
2023-09-08 13:39:55 +0900

    style: 이미지 폴더 변수명 수정

--- frontend/src/hooks/common/useImageUploader.ts
+++ frontend/src/hooks/common/useImageUploader.ts
@@ -5,7 +5,7 @@ import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 const MAX_SIZE = 5 * 1024 * 1024;
 const client = new S3Client({});
-const IMAGE_DIRECTORY = window.location.href.includes('dev') ? 'dev' : 'prod';
+const IMAGE_ENVIRONMENT = window.location.href.includes('dev') ? 'dev' : 'prod';
 
 const useImageUploader = () => {
   const [previewImage, setPreviewImage] = useState('');
@@ -27,13 +27,13 @@ const useImageUploader = () => {
     try {
       const image = new PutObjectCommand({
         Bucket: process.env.S3_BUCKET_NAME,
-        Key: `${process.env.S3_DIRECTORY}/${IMAGE_DIRECTORY}/${imageFile.name}`,
+        Key: `${process.env.S3_DIRECTORY}/${IMAGE_ENVIRONMENT}/${imageFile.name}`,
         Body: imageFile,
       });
 
       setPreviewImage(URL.createObjectURL(imageFile));
       await client.send(image);
-      setImageUrl(`${process.env.CLOUDFRONT_URL}/${IMAGE_DIRECTORY}/${imageFile.name}`);
+      setImageUrl(`${process.env.CLOUDFRONT_URL}/${IMAGE_ENVIRONMENT}/${imageFile.name}`);
     } catch (error) {
       alert('이미지 업로드에 실패했습니다. 다시 시도해주세요.');
     }
