d19a7f38cd66dbe722c77fe1414904f246f27444
wugawuga <dnr3655@naver.com>
2023-09-10 22:28:15 +0900

    fix: test 실패 수정

--- backend/src/main/java/com/funeat/common/controller/PreSingedApiController.java
+++ backend/src/main/java/com/funeat/common/controller/PreSingedApiController.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PreSingedApiController implements PreSingedController {
 
     private final S3UploadUrlGenerator s3UploadUrlGenerator;
