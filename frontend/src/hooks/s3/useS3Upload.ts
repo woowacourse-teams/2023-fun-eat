9d6e63807ea633a554aa20318ad78a553753e9a9
Leejin-Yang <ejin1996@gmail.com>
2023-09-10 23:43:53 +0900

    refactor: S3 업로드 예외처리 추가

--- frontend/src/hooks/s3/useS3Upload.ts
+++ frontend/src/hooks/s3/useS3Upload.ts
@@ -3,6 +3,7 @@ import { usePresignedMutation } from '../queries/s3';
 import type { PresignedResponse } from '@/types/s3';
 
 const ENVIRONMENT = window.location.href.includes('dev') ? 'dev' : 'prod';
+const UPLOAD_ERROR_MESSAGE = '이미지 업로드에 실패했습니다. 다시 시도해주세요.';
 
 const putFileToS3 = (url: string, file: File) => {
   fetch(url, {
@@ -27,9 +28,13 @@ const useS3Upload = (file: File | null) => {
   const fileUrl = file !== null ? `${process.env.CLOUDFRONT_URL}/${ENVIRONMENT}/${file.name}` : null;
 
   const uploadToS3 = async () => {
-    if (file !== null) {
-      const preSignedUrl = await getPreSignedUrl(file);
-      putFileToS3(preSignedUrl, file);
+    try {
+      if (file !== null) {
+        const preSignedUrl = await getPreSignedUrl(file);
+        putFileToS3(preSignedUrl, file);
+      }
+    } catch {
+      throw new Error(UPLOAD_ERROR_MESSAGE);
     }
   };
 
