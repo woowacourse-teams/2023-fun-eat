3cb58777c90d50bce93b6e254c6e8d9148627a55
Leejin-Yang <ejin1996@gmail.com>
2023-09-11 15:57:36 +0900

    feat: 이미지 파일 타입 예외처리 추가

--- frontend/src/hooks/common/useImageUploader.ts
+++ frontend/src/hooks/common/useImageUploader.ts
@@ -2,19 +2,26 @@ import { useState } from 'react';
 
 import { uuid } from '@/utils/uuid';
 
+const isImageFile = (file: File) => file.type !== 'image/png' && file.type !== 'image/jpeg';
+
+const editImageFileName = (imageFile: File) => {
+  const fileName = imageFile.name;
+  const fileExtension = fileName.split('.').pop();
+  const newFileName = `${uuid()}.${fileExtension}`;
+
+  return new File([imageFile], newFileName, { type: imageFile.type });
+};
+
 const useImageUploader = () => {
   const [imageFile, setImageFile] = useState<File | null>(null);
   const [previewImage, setPreviewImage] = useState('');
 
-  const editImageFileName = (imageFile: File) => {
-    const fileName = imageFile.name;
-    const fileExtension = fileName.split('.').pop();
-    const newFileName = `${uuid()}.${fileExtension}`;
-
-    return new File([imageFile], newFileName, { type: imageFile.type });
-  };
-
   const uploadImage = (imageFile: File) => {
+    if (isImageFile(imageFile)) {
+      alert('이미지 파일만 업로드 가능합니다.');
+      return;
+    }
+
     const editedImageFile = editImageFileName(imageFile);
 
     setPreviewImage(URL.createObjectURL(editedImageFile));
