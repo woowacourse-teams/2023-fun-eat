3d935a676446fe2cc63f9906f977890b0dbb9c71
TaeeunKim <xodms0309@naver.com>
2023-09-13 20:08:45 +0900

    feat: 스크롤 이벤트에 디바운스 적용

--- frontend/src/hooks/common/useScrollRestoration.ts
+++ frontend/src/hooks/common/useScrollRestoration.ts
@@ -1,22 +1,28 @@
 import { useEffect } from 'react';
 
+import useTimeout from './useTimeout';
 import { useCategoryContext } from '../context';
 
 const useScrollRestoration = (currentCategoryId: number) => {
   const { saveCurrentTabScroll } = useCategoryContext();
 
+  const handleScroll = () => {
+    const mainElement = document.getElementById('main');
+    if (!mainElement) return;
+
+    saveCurrentTabScroll(currentCategoryId, mainElement.scrollTop);
+  };
+
+  const [timeoutFn] = useTimeout(handleScroll, 300);
+
   useEffect(() => {
     const mainElement = document.getElementById('main');
     if (!mainElement) return;
 
-    const handleScroll = () => {
-      saveCurrentTabScroll(currentCategoryId, mainElement?.scrollTop);
-    };
-
-    mainElement.addEventListener('scroll', handleScroll);
+    mainElement.addEventListener('scroll', timeoutFn);
 
     return () => {
-      mainElement.removeEventListener('scroll', handleScroll);
+      mainElement.removeEventListener('scroll', timeoutFn);
     };
   }, [currentCategoryId]);
 };
