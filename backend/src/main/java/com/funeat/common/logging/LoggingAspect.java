bdc31c8da5478b0290b61567f343724eaa40b401
70825 <hello70825@gmail.com>
2023-09-14 10:58:59 +0900

    refactor: 요청, 응답 로깅 내용을 더 가독성 있게 수정 기존: 컨트롤러 메서드를 로깅함 수정: [요청] 요청 메서드, 요청 경로, 요청 데이터, [응답] 응답 상태코드, 응답 데이터

--- backend/src/main/java/com/funeat/common/logging/LoggingAspect.java
+++ backend/src/main/java/com/funeat/common/logging/LoggingAspect.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -15,6 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Aspect
 @Component
@@ -35,12 +36,30 @@ public class LoggingAspect {
 
     @Before("allPresentation() && logging()")
     public void requestLogging(final JoinPoint joinPoint) {
-        final Signature signature = joinPoint.getSignature();
-        final String classAndMethodName = signature.toShortString();
-
+        final Class<?> clazz = joinPoint.getTarget().getClass();
         final Map<String, Object> args = getSpecificParameters(joinPoint);
 
-        printLog("[REQUEST] method={} args={}", classAndMethodName, args);
+        printRequestLog(clazz, args);
+    }
+
+    private String getRequestMethod(final Class<?> clazz) {
+        final RequestMapping requestMapping = clazz.getDeclaredAnnotation(RequestMapping.class);
+        final RequestMethod[] requestMethods = requestMapping.method();
+        if (requestMethods.length > 0) {
+            return requestMethods[0].name();
+        }
+        log.warn("[LOGGING ERROR] Request Method가 존재하지 않습니다.");
+        return "";
+    }
+
+    private String getRequestUrl(final Class<?> clazz) {
+        final RequestMapping requestMapping = clazz.getDeclaredAnnotation(RequestMapping.class);
+        final String[] requestUrls = requestMapping.path();
+        if (requestUrls.length > 0) {
+            return requestUrls[0];
+        }
+        log.warn("[LOGGING ERROR] Request URL이 존재하지 않습니다.");
+        return "";
     }
 
     private Map<String, Object> getSpecificParameters(final JoinPoint joinPoint) {
@@ -58,20 +77,27 @@ public class LoggingAspect {
         return params;
     }
 
-    private void printLog(final String logFormat, final String classAndMethodName, final Object value) {
+    private void printRequestLog(final Class<?> clazz, final Object value) {
         try {
-            log.info(logFormat, classAndMethodName, objectMapper.writeValueAsString(value));
+            final String requestMethod = getRequestMethod(clazz);
+            final String requestUrl = getRequestUrl(clazz);
+            log.info("[REQUEST] {}, {}, {}", requestMethod, requestUrl, objectMapper.writeValueAsString(value));
         } catch (final JsonProcessingException e) {
-            log.warn("로깅에 실패했습니다");
+            log.warn("[LOGGING ERROR] Request 로깅에 실패했습니다");
         }
     }
 
     @AfterReturning(value = "allPresentation() && logging()", returning = "responseEntity")
     public void requestLogging(final JoinPoint joinPoint, final ResponseEntity<?> responseEntity) {
-        final Signature signature = joinPoint.getSignature();
-        final String classAndMethodName = signature.toShortString();
-
-        printLog("[RESPONSE] method={} responseBody={}", classAndMethodName, responseEntity.getClass());
+        printResponseLog(responseEntity);
     }
 
+    private void printResponseLog(final ResponseEntity<?> responseEntity) {
+        try {
+            final String responseStatus = responseEntity.getStatusCode().toString();
+            log.info("[RESPONSE] {} {}", responseStatus, objectMapper.writeValueAsString(responseEntity.getBody()));
+        } catch (final JsonProcessingException e) {
+            log.warn("[LOGGING ERROR] Response 로깅에 실패했습니다");
+        }
+    }
 }
