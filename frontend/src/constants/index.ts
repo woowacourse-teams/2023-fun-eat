7632b127693812821cb89db10c8fce3886c78d29
hae-on <solbi2004@naver.com>
2023-09-14 10:27:53 +0900

    chore: 이미지 s3로 이동

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -68,3 +68,19 @@ export const CATEGORY_TYPE = {
 export const IMAGE_MAX_SIZE = 5 * 1024 * 1024;
 
 export const ENVIRONMENT = window.location.href.includes('dev') ? 'dev' : 'prod';
+
+export const IMAGE_URL = process.env.CLOUDFRONT_URL;
+
+export const CATEGORY_NAME = ['간편식사', '과자류', '아이스크림', '식품', '음료'] as const;
+
+export const STORE_NAMES = ['CU', 'GS25', '이마트24', '세븐일레븐'] as const;
+
+export const CATEGORY_IMAGES = [
+  `${IMAGE_URL}/${ENVIRONMENT}/food.jpeg`,
+  `${IMAGE_URL}/${ENVIRONMENT}/snack.jpeg`,
+  `${IMAGE_URL}/${ENVIRONMENT}/icecream.jpeg`,
+  `${IMAGE_URL}/${ENVIRONMENT}/ramen.jpeg`,
+  `${IMAGE_URL}/${ENVIRONMENT}/tea.jpeg`,
+];
+
+export const STORE_IMAGES = [];
