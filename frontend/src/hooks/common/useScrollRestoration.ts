ad80ff811948c6ac02e50848bdda1bcdba8eafaf
TaeeunKim <xodms0309@naver.com>
2023-09-14 15:59:22 +0900

    style: 중괄호 추가

--- frontend/src/hooks/common/useScrollRestoration.ts
+++ frontend/src/hooks/common/useScrollRestoration.ts
@@ -9,14 +9,18 @@ const useScrollRestoration = (currentCategoryId: number, ref: RefObject<HTMLElem
   const { currentTabScroll } = useCategoryValueContext();
 
   const handleScroll = () => {
-    if (!ref.current) return;
+    if (!ref.current) {
+      return;
+    }
     saveCurrentTabScroll(currentCategoryId, ref.current.scrollTop);
   };
 
   const [timeoutFn] = useTimeout(handleScroll, 300);
 
   useEffect(() => {
-    if (!ref.current) return;
+    if (!ref.current) {
+      return;
+    }
 
     ref.current.addEventListener('scroll', timeoutFn);
 
