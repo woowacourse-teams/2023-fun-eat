377a99324e55b490d7486069e224580d72cb3349
TaeeunKim <xodms0309@naver.com>
2023-09-15 14:34:57 +0900

    feat: 마이페이지에서도 스크롤 버튼 추가

--- frontend/src/pages/MemberRecipePage.tsx
+++ frontend/src/pages/MemberRecipePage.tsx
@@ -1,15 +1,17 @@
 import { Spacing } from '@fun-eat/design-system';
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
-import { Suspense } from 'react';
+import { Suspense, useRef } from 'react';
+import styled from 'styled-components';
 
 import { ErrorBoundary, ErrorComponent, Loading, ScrollButton, SectionTitle } from '@/components/Common';
 import { MemberRecipeList } from '@/components/Members';
 
 const MemberRecipePage = () => {
   const { reset } = useQueryErrorResetBoundary();
+  const memberRecipeRef = useRef<HTMLDivElement>(null);
 
   return (
-    <>
+    <MebmberRecipePageContainer ref={memberRecipeRef}>
       <SectionTitle name="내가 작성한 꿀조합" />
       <Spacing size={18} />
       <ErrorBoundary fallback={ErrorComponent} handleReset={reset}>
@@ -17,9 +19,14 @@ const MemberRecipePage = () => {
           <MemberRecipeList />
         </Suspense>
       </ErrorBoundary>
-      <ScrollButton />
-    </>
+      <ScrollButton targetRef={memberRecipeRef} />
+    </MebmberRecipePageContainer>
   );
 };
 
 export default MemberRecipePage;
+
+const MebmberRecipePageContainer = styled.div`
+  height: 100%;
+  overflow-y: auto;
+`;
--- frontend/src/pages/MemberReviewPage.tsx
+++ frontend/src/pages/MemberReviewPage.tsx
@@ -1,15 +1,17 @@
 import { Spacing } from '@fun-eat/design-system';
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
-import { Suspense } from 'react';
+import { Suspense, useRef } from 'react';
+import styled from 'styled-components';
 
 import { ErrorBoundary, ErrorComponent, Loading, ScrollButton, SectionTitle } from '@/components/Common';
 import { MemberReviewList } from '@/components/Members';
 
 const MemberReviewPage = () => {
   const { reset } = useQueryErrorResetBoundary();
+  const memberReviewRef = useRef<HTMLDivElement>(null);
 
   return (
-    <>
+    <MebmberReviewPageContainer ref={memberReviewRef}>
       <SectionTitle name="내가 작성한 리뷰" />
       <Spacing size={18} />
       <ErrorBoundary fallback={ErrorComponent} handleReset={reset}>
@@ -17,9 +19,14 @@ const MemberReviewPage = () => {
           <MemberReviewList />
         </Suspense>
       </ErrorBoundary>
-      <ScrollButton />
-    </>
+      <ScrollButton targetRef={memberReviewRef} />
+    </MebmberReviewPageContainer>
   );
 };
 
 export default MemberReviewPage;
+
+const MebmberReviewPageContainer = styled.div`
+  height: 100%;
+  overflow-y: auto;
+`;
