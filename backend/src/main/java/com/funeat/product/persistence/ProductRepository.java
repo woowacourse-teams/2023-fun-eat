1055b4cd9fe4924b91bf7f35d6a6de56e04063e9
hanueleee <ezzeezz@naver.com>
2023-09-19 22:09:23 +0900

    refactor: ProductRepository의 findAllByCategory와 findAllByCategoryByReviewCountDesc 메소드를 findAllByCategory메소드로 통합 및 반환타입 수정
    - ReviewCount 반정규화로 인해 메소드 분리 필요성 없어짐
    - ReviewCount를 위한 join 쿼리 제거
    - 페이징에 대한 자세한 정보(ex. 전체 페이지 수등) 필요없기 때문에 반환값을 Page에서 Slice로 수정

--- backend/src/main/java/com/funeat/product/persistence/ProductRepository.java
+++ backend/src/main/java/com/funeat/product/persistence/ProductRepository.java
@@ -7,29 +7,17 @@ import com.funeat.product.dto.ProductReviewCountDto;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query(value = "SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) "
+    @Query(value = "SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, p.reviewCount) "
             + "FROM Product p "
-            + "LEFT JOIN p.reviews r "
-            + "WHERE p.category = :category "
-            + "GROUP BY p ",
-            countQuery = "SELECT COUNT(p) FROM Product p WHERE p.category = :category")
-    Page<ProductInCategoryDto> findAllByCategory(@Param("category") final Category category, final Pageable pageable);
-
-    @Query(value = "SELECT new com.funeat.product.dto.ProductInCategoryDto(p.id, p.name, p.price, p.image, p.averageRating, COUNT(r)) "
-                    + "FROM Product p "
-                    + "LEFT JOIN p.reviews r "
-                    + "WHERE p.category = :category "
-                    + "GROUP BY p "
-                    + "ORDER BY COUNT(r) DESC, p.id DESC ",
-            countQuery = "SELECT COUNT(p) FROM Product p WHERE p.category = :category")
-    Page<ProductInCategoryDto> findAllByCategoryOrderByReviewCountDesc(@Param("category") final Category category,
-                                                                       final Pageable pageable);
+            + "WHERE p.category = :category ")
+    Slice<ProductInCategoryDto> findAllByCategory(@Param("category") final Category category, final Pageable pageable);
 
     @Query("SELECT new com.funeat.product.dto.ProductReviewCountDto(p, COUNT(r.id)) "
             + "FROM Product p "
