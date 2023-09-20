2bb9d223f4e0574c11bfcf57217b37b2b0935238
Leejin-Yang <ejin1996@gmail.com>
2023-09-20 13:44:29 +0900

    refactor: 스피너 애니메이션 수정

--- frontend/src/components/Common/Loading/Loading.tsx
+++ frontend/src/components/Common/Loading/Loading.tsx
@@ -38,20 +38,12 @@ const rotate = keyframes`
   0% {
     transform: rotate(0deg);
   }
-  25% {
-    transform: rotate(-90deg);
-  }
-  50% {
-    transform: rotate(0deg);
-  }
-  75% {
-    transform: rotate(90deg);
-  }
+
   100% {
-    transform: rotate(0deg);
+    transform: rotate(-360deg);
   }
 `;
 
 const PlateImageWrapper = styled.div`
-  animation: ${rotate} 1s linear infinite;
+  animation: ${rotate} 1.5s ease-in-out infinite;
 `;
