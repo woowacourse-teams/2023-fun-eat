7d0cc0146eaa888f2a5c93562f9249ff57aaa5f4
sᴏʟʙɪ ☔️ <solbi2004@naver.com>
2023-09-21 11:32:24 +0900

    [FE] feat: 폰트 추가 (#672)
    * feat: 폰트 추가하고 preload 설정
    
    * chore: font family 추가

--- frontend/src/styles/index.ts
+++ frontend/src/styles/index.ts
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
+import fonts from './font';
+
 const GlobalStyle = createGlobalStyle`
+${fonts}
+
   #root {
     position: absolute;
     top: 0;
