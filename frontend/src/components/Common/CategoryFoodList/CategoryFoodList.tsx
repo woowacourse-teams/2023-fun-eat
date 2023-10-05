4c99360655cefd74daa0d8f9016768e7aaf6f25c
Leejin-Yang <ejin1996@gmail.com>
2023-10-06 00:05:17 +0900

    feat: 홈 배너 및 카테고리 링크 GA 이벤트 추가

--- frontend/src/components/Common/CategoryFoodList/CategoryFoodList.tsx
+++ frontend/src/components/Common/CategoryFoodList/CategoryFoodList.tsx
@@ -5,18 +5,33 @@ import styled from 'styled-components';
 import CategoryItem from '../CategoryItem/CategoryItem';
 
 import { CATEGORY_TYPE } from '@/constants';
+import { useGA } from '@/hooks/common';
 import { useCategoryFoodQuery } from '@/hooks/queries/product';
 
 const category = CATEGORY_TYPE.FOOD;
 
 const CategoryFoodList = () => {
   const { data: categories } = useCategoryFoodQuery(category);
+  const { gaEvent } = useGA();
+
+  const handleHomeCategoryLinkClick = (categoryName: string) => {
+    gaEvent({
+      category: 'link',
+      action: `${categoryName} 카테고리 링크 클릭`,
+      label: '카테고리',
+    });
+  };
 
   return (
     <div>
       <CategoryFoodListWrapper>
         {categories.map((menu) => (
-          <Link key={menu.id} as={RouterLink} to={`products/food?category=${menu.id}`}>
+          <Link
+            key={menu.id}
+            as={RouterLink}
+            to={`products/food?category=${menu.id}`}
+            onClick={() => handleHomeCategoryLinkClick(menu.name)}
+          >
             <CategoryItem name={menu.name} image={menu.image} />
           </Link>
         ))}
