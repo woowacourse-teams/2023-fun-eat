a1a27dca8aef4b656eee5865d0b5635759002233
sᴏʟʙɪ ☔️ <solbi2004@naver.com>
2023-10-06 15:47:55 +0900

    [FE] feat: 토스트 구현 (#608)
    * feat: 토스트 구현
    
    * feat: portal 구현
    
    * chore: 파일 이름 변경
    
    * chore: 변경된 파일 이름에 따라 import명 변경
    
    * refactor: animation 분리
    
    * feat: 에러일 때 스타일 추가
    
    * refactor: 기본, 에러 버전 구현
    
    * refactor: 리뷰 반영
    
    * refactor: toast context로 변경
    
    * refactor: context action과 value로 분리
    
    * refactor: toast context에서 portal 실행되도록 변경
    
    * refactor: portal 위치 변경
    
    * refactor: context를 사용해 스토리북 변경
    
    * chore: 필요없는 fragment 삭제
    
    * refactor: 구조 분해 할당으로 변경
    
    * refactor: style 이름 변경
    
    * refactor: 사용하지 않는 action 삭제
    
    * refactor: useToast로 분리
    
    Co-authored-by: Leejin Yang <ejin1996@gmail.com>
    
    * chore: 사용하지 않는 테스트 버튼 삭제
    
    ---------
    
    Co-authored-by: Leejin Yang <ejin1996@gmail.com>

--- frontend/src/hooks/context/index.ts
+++ frontend/src/hooks/context/index.ts
@@ -4,3 +4,5 @@ export { default as useReviewFormActionContext } from './useReviewFormActionCont
 export { default as useReviewFormValueContext } from './useReviewFormValueContext';
 export { default as useRecipeFormActionContext } from './useRecipeFormActionContext';
 export { default as useRecipeFormValueContext } from './useRecipeFormValueContext';
+export { default as useToastActionContext } from './useToastActionContext';
+export { default as useToastValueContext } from './useToastValueContext';
