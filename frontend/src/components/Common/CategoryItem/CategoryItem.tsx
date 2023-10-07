66c4391071f7d876c2b37a61a830e0a4a2f3400a
TaeeunKim <xodms0309@naver.com>
2023-10-07 16:54:16 +0900

    feat: categoryItem에 GA 이벤트 추가

--- frontend/src/components/Common/CategoryItem/CategoryItem.tsx
+++ frontend/src/components/Common/CategoryItem/CategoryItem.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { PATH } from '@/constants/path';
+import { useGA } from '@/hooks/common';
 import { useCategoryActionContext } from '@/hooks/context';
 
 interface CategoryItemProps {
@@ -16,9 +17,17 @@ const CategoryItem = ({ categoryId, name, image, categoryType }: CategoryItemPro
   const navigate = useNavigate();
   const { selectCategory } = useCategoryActionContext();
 
+  const { gaEvent } = useGA();
+
   const handleCategoryItemClick = (categoryId: number) => {
     selectCategory(categoryType, categoryId);
     navigate(PATH.PRODUCT_LIST + '/' + categoryType);
+
+    gaEvent({
+      category: 'button',
+      action: `${name} 카테고리 링크 클릭`,
+      label: '카테고리',
+    });
   };
 
   return (
