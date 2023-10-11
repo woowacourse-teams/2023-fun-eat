d7413c3017429b75c98811434c70fba0eb7ca3f5
Go-Jaecheol <gojaech@naver.com>
2023-10-11 10:38:32 +0900

    style: import 와일드카드 제거

--- backend/src/main/java/com/funeat/review/domain/Review.java
+++ backend/src/main/java/com/funeat/review/domain/Review.java
@@ -4,7 +4,15 @@ import com.funeat.member.domain.Member;
 import com.funeat.member.domain.favorite.ReviewFavorite;
 import com.funeat.product.domain.Product;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
