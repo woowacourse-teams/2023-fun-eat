d18886bfced9e6630ba3cf8fa91744f780a1ccbd
hanueleee <ezzeezz@naver.com>
2023-10-13 00:13:37 +0900

    test: 리뷰 삭제 이벤트 관련 테스트 보완

--- backend/src/test/java/com/funeat/review/application/ReviewDeleteEventListenerTest.java
+++ backend/src/test/java/com/funeat/review/application/ReviewDeleteEventListenerTest.java
@@ -10,11 +10,15 @@ import static com.funeat.fixture.TagFixture.태그_맛있어요_TASTE_생성;
 import static com.funeat.fixture.TagFixture.태그_아침식사_ETC_생성;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
 import com.funeat.common.EventTest;
 import com.funeat.common.ImageUploader;
+import com.funeat.common.exception.CommonException;
+import com.funeat.common.exception.CommonException.S3DeleteFailException;
+import com.funeat.exception.CommonErrorCode;
 import com.funeat.tag.domain.Tag;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,7 +33,7 @@ class ReviewDeleteEventListenerTest extends EventTest {
     ImageUploader uploader;
 
     @Nested
-    class 리뷰_삭제_이벤트_발행_유무 {
+    class 리뷰_삭제_이벤트_발행 {
 
         @Test
         void 리뷰_작성자가_리뷰_삭제_시도시_리뷰_삭제_이벤트가_발행된다() {
@@ -104,7 +108,7 @@ class ReviewDeleteEventListenerTest extends EventTest {
     }
 
     @Nested
-    class 이미지_삭제_로직_작동_유무 {
+    class 이미지_삭제_로직_작동 {
 
         @Test
         void 리뷰_삭제가_정상적으로_커밋되고_이미지가_존재하면_이미지_삭제_로직이_작동한다() {
@@ -167,6 +171,42 @@ class ReviewDeleteEventListenerTest extends EventTest {
 
             // then
             verify(uploader, timeout(100).times(0)).delete(any());
+        }
+
+        @Test
+        void 이미지_삭제_로직이_실패해도_메인로직까지_롤백되어서는_안된다() {
+            // given
+            final var member = 멤버_멤버1_생성();
+            final var memberId = 단일_멤버_저장(member);
+
+            final var category = 카테고리_즉석조리_생성();
+            단일_카테고리_저장(category);
+
+            final var product = 상품_삼각김밥_가격1000원_평점2점_생성(category);
+            final var productId = 단일_상품_저장(product);
+
+            final var tag1 = 태그_맛있어요_TASTE_생성();
+            final var tag2 = 태그_아침식사_ETC_생성();
+            복수_태그_저장(tag1, tag2);
+
+            final var tagIds = 태그_아이디_변환(tag1, tag2);
+            final var image = 이미지_생성();
+
+            final var request = 리뷰추가요청_재구매O_생성(4L, tagIds);
+            reviewService.create(productId, memberId, image, request);
+
+            final var review = reviewRepository.findAll().get(0);
+            final var reviewId = review.getId();
+
+            doThrow(new S3DeleteFailException(CommonErrorCode.UNKNOWN_SERVER_ERROR_CODE))
+                    .when(uploader)
+                    .delete(any());
+
+            // when
+            reviewService.deleteReview(reviewId, memberId);
+
+            // then
+            assertThat(reviewRepository.findById(reviewId)).isEmpty();
         }
     }
 
