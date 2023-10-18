d7c357d347bb363de860d02dfe107fb8feb19bc8
wugawuga <dnr3655@naver.com>
2023-10-18 21:06:06 +0900

    remove: 리뷰 비동기 테스트 삭제

--- backend/src/test/java/com/funeat/review/application/ReviewDeleteEventListenerTest.java
+++ backend/src/test/java/com/funeat/review/application/ReviewDeleteEventListenerTest.java
@@ -6,14 +6,10 @@ import static com.funeat.fixture.MemberFixture.멤버_멤버2_생성;
 import static com.funeat.fixture.ProductFixture.상품_삼각김밥_가격1000원_평점2점_생성;
 import static com.funeat.fixture.ReviewFixture.리뷰_이미지test1_평점1점_재구매O_생성;
 import static com.funeat.fixture.ReviewFixture.리뷰_이미지test2_평점2점_재구매O_생성;
-import static com.funeat.fixture.ReviewFixture.리뷰_이미지test3_평점3점_재구매O_생성;
 import static com.funeat.fixture.ReviewFixture.리뷰_이미지test4_평점4점_재구매O_생성;
-import static com.funeat.fixture.ReviewFixture.리뷰_이미지없음_평점1점_재구매X_생성;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.timeout;
-import static org.mockito.Mockito.verify;
 
 import com.funeat.common.EventTest;
 import com.funeat.common.ImageUploader;
@@ -87,48 +83,6 @@ class ReviewDeleteEventListenerTest extends EventTest {
     class 이미지_삭제_로직_작동 {
 
         @Test
-        void 리뷰_삭제가_정상적으로_커밋되고_이미지가_존재하면_이미지_삭제_로직이_작동한다() {
-            // given
-            final var author = 멤버_멤버1_생성();
-            final var authorId = 단일_멤버_저장(author);
-
-            final var category = 카테고리_즉석조리_생성();
-            단일_카테고리_저장(category);
-
-            final var product = 상품_삼각김밥_가격1000원_평점2점_생성(category);
-            단일_상품_저장(product);
-
-            final var review = reviewRepository.save(리뷰_이미지test3_평점3점_재구매O_생성(author, product, 0L));
-
-            // when
-            reviewService.deleteReview(review.getId(), authorId);
-
-            // then
-            verify(uploader, timeout(1000).times(1)).delete(any());
-        }
-
-        @Test
-        void 리뷰_삭제가_정상적으로_커밋되었지만_이미지가_존재하지_않으면_이미지_삭제_로직이_작동하지않는다() {
-            // given
-            final var author = 멤버_멤버1_생성();
-            final var authorId = 단일_멤버_저장(author);
-
-            final var category = 카테고리_즉석조리_생성();
-            단일_카테고리_저장(category);
-
-            final var product = 상품_삼각김밥_가격1000원_평점2점_생성(category);
-            단일_상품_저장(product);
-
-            final var review = reviewRepository.save(리뷰_이미지없음_평점1점_재구매X_생성(author, product, 0L));
-
-            // when
-            reviewService.deleteReview(review.getId(), authorId);
-
-            // then
-            verify(uploader, timeout(1000).times(0)).delete(any());
-        }
-
-        @Test
         void 이미지_삭제_로직이_실패해도_메인로직까지_롤백되어서는_안된다() {
             // given
             final var author = 멤버_멤버1_생성();
@@ -143,8 +97,8 @@ class ReviewDeleteEventListenerTest extends EventTest {
             final var review = reviewRepository.save(리뷰_이미지test4_평점4점_재구매O_생성(author, product, 0L));
 
             doThrow(new S3DeleteFailException(CommonErrorCode.UNKNOWN_SERVER_ERROR_CODE))
-                    .when(uploader)
-                    .delete(any());
+                .when(uploader)
+                .delete(any());
 
             // when
             reviewService.deleteReview(review.getId(), authorId);
