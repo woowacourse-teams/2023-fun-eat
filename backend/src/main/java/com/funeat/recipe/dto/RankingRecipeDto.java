e214528724e569718b0e120f10340c9990d9319f
JFe <33208246+Go-Jaecheol@users.noreply.github.com>
2023-10-19 19:53:39 +0900

    [BE] refactor: 리뷰 및 꿀조합 랭킹 Response 수정 (#821)
    * refactor: RankingReviewDto에 createdAt 추가
    
    * refactor: RankingRecipeDto에 createdAt 추가

--- backend/src/main/java/com/funeat/recipe/dto/RankingRecipeDto.java
+++ backend/src/main/java/com/funeat/recipe/dto/RankingRecipeDto.java
@@ -2,6 +2,7 @@ package com.funeat.recipe.dto;
 
 import com.funeat.recipe.domain.Recipe;
 import com.funeat.recipe.domain.RecipeImage;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class RankingRecipeDto {
@@ -11,23 +12,26 @@ public class RankingRecipeDto {
     private final String title;
     private final RecipeAuthorDto author;
     private final Long favoriteCount;
+    private final LocalDateTime createdAt;
 
     public RankingRecipeDto(final Long id, final String image, final String title, final RecipeAuthorDto author,
-                            final Long favoriteCount) {
+                            final Long favoriteCount, final LocalDateTime createdAt) {
         this.id = id;
         this.image = image;
         this.title = title;
         this.author = author;
         this.favoriteCount = favoriteCount;
+        this.createdAt = createdAt;
     }
 
     public static RankingRecipeDto toDto(final Recipe recipe, final List<RecipeImage> images,
                                          final RecipeAuthorDto author) {
         if (images.isEmpty()) {
-            return new RankingRecipeDto(recipe.getId(), null, recipe.getTitle(), author, recipe.getFavoriteCount());
+            return new RankingRecipeDto(recipe.getId(), null, recipe.getTitle(), author,
+                    recipe.getFavoriteCount(), recipe.getCreatedAt());
         }
         return new RankingRecipeDto(recipe.getId(), images.get(0).getImage(), recipe.getTitle(), author,
-                recipe.getFavoriteCount());
+                recipe.getFavoriteCount(), recipe.getCreatedAt());
     }
 
     public Long getId() {
@@ -48,5 +52,9 @@ public class RankingRecipeDto {
 
     public Long getFavoriteCount() {
         return favoriteCount;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 }
